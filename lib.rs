a28528559eb91c18e5ac8ef3161d522b5243b9ef
Sean Bowe <ewillbefull@gmail.com>
2018-04-08 13:49:01 -0600

    Simplify API and improve performance

--- lib.rs
+++ lib.rs
@@ -708,12 +708,14 @@ impl MPCParameters {
     /// sure their contribution is in the final parameters, by
     /// checking to see if it appears in the output of
     /// `MPCParameters::verify`.
-    pub fn contribute(
+    pub fn contribute<R: Rng>(
         &mut self,
-        pubkey: &PublicKey,
-        privkey: &PrivateKey
+        rng: &mut R
     ) -> [u8; 64]
     {
+        // Generate a keypair
+        let (pubkey, privkey) = keypair(rng, self);
+
         fn batch_exp<C: CurveAffine>(bases: &mut [C], coeff: C::Scalar) {
             let coeff = coeff.into_repr();
 
@@ -963,7 +965,7 @@ impl MPCParameters {
 /// This allows others to verify that you contributed. The hash produced
 /// by `MPCParameters::contribute` is just a BLAKE2b hash of this object.
 #[derive(Clone)]
-pub struct PublicKey {
+struct PublicKey {
     /// This is the delta (in G1) after the transformation, kept so that we
     /// can check correctness of the public keys without having the entire
     /// interstitial parameters for each contribution.
@@ -1052,12 +1054,8 @@ impl PartialEq for PublicKey {
 }
 
 /// Verify a contribution, given the old parameters and
-/// the new parameters. This is basically a wrapper around
-/// `MPCParameters::verify` which just checks that a new
-/// contribution was added and none of the existing
-/// contributions were changed.
-pub fn verify_contribution<C: Circuit<Bls12>>(
-    circuit: C,
+/// the new parameters. Returns the hash of the contribution.
+pub fn verify_contribution(
     before: &MPCParameters,
     after: &MPCParameters
 ) -> Result<[u8; 64], ()>
@@ -1072,7 +1070,119 @@ pub fn verify_contribution<C: Circuit<Bls12>>(
         return Err(());
     }
 
-    after.verify(circuit).map(|v| *v.last().unwrap())
+    // H/L will change, but should have same length
+    if before.params.h.len() != after.params.h.len() {
+        return Err(());
+    }
+    if before.params.l.len() != after.params.l.len() {
+        return Err(());
+    }
+
+    // A/B_G1/B_G2 doesn't change at all
+    if before.params.a != after.params.a {
+        return Err(());
+    }
+    if before.params.b_g1 != after.params.b_g1 {
+        return Err(());
+    }
+    if before.params.b_g2 != after.params.b_g2 {
+        return Err(());
+    }
+
+    // alpha/beta/gamma don't change
+    if before.params.vk.alpha_g1 != after.params.vk.alpha_g1 {
+        return Err(());
+    }
+    if before.params.vk.beta_g1 != after.params.vk.beta_g1 {
+        return Err(());
+    }
+    if before.params.vk.beta_g2 != after.params.vk.beta_g2 {
+        return Err(());
+    }
+    if before.params.vk.gamma_g2 != after.params.vk.gamma_g2 {
+        return Err(());
+    }
+
+    // IC shouldn't change, as gamma doesn't change
+    if before.params.vk.ic != after.params.vk.ic {
+        return Err(());
+    }
+
+    // cs_hash should be the same
+    if &before.cs_hash[..] != &after.cs_hash[..] {
+        return Err(());
+    }
+
+    let sink = io::sink();
+    let mut sink = HashWriter::new(sink);
+    sink.write_all(&before.cs_hash[..]).unwrap();
+
+    for pubkey in &before.contributions {
+        sink.write_all(pubkey.delta_after.into_uncompressed().as_ref()).unwrap();
+    }
+
+    let pubkey = after.contributions.last().unwrap();
+    sink.write_all(pubkey.s.into_uncompressed().as_ref()).unwrap();
+    sink.write_all(pubkey.s_delta.into_uncompressed().as_ref()).unwrap();
+
+    let h = sink.into_hash();
+
+    // The transcript must be consistent
+    if &pubkey.transcript[..] != h.as_ref() {
+        return Err(());
+    }
+
+    let r = hash_to_g2(h.as_ref()).into_affine();
+
+    // Check the signature of knowledge
+    if !same_ratio((r, pubkey.r_delta), (pubkey.s, pubkey.s_delta)) {
+        return Err(());
+    }
+
+    // Check the change from the old delta is consistent
+    if !same_ratio(
+        (before.params.vk.delta_g1, pubkey.delta_after),
+        (r, pubkey.r_delta)
+    ) {
+        return Err(());
+    }
+
+    // Current parameters should have consistent delta in G1
+    if pubkey.delta_after != after.params.vk.delta_g1 {
+        return Err(());
+    }
+
+    // Current parameters should have consistent delta in G2
+    if !same_ratio(
+        (G1Affine::one(), pubkey.delta_after),
+        (G2Affine::one(), after.params.vk.delta_g2)
+    ) {
+        return Err(());
+    }
+
+    // H and L queries should be updated with delta^-1
+    if !same_ratio(
+        merge_pairs(&before.params.h, &after.params.h),
+        (after.params.vk.delta_g2, before.params.vk.delta_g2) // reversed for inverse
+    ) {
+        return Err(());
+    }
+
+    if !same_ratio(
+        merge_pairs(&before.params.l, &after.params.l),
+        (after.params.vk.delta_g2, before.params.vk.delta_g2) // reversed for inverse
+    ) {
+        return Err(());
+    }
+
+    let sink = io::sink();
+    let mut sink = HashWriter::new(sink);
+    pubkey.write(&mut sink).unwrap();
+    let h = sink.into_hash();
+    let mut response = [0u8; 64];
+    response.copy_from_slice(h.as_ref());
+
+    Ok(response)
 }
 
 /// Checks if pairs have the same ratio.
@@ -1147,14 +1257,14 @@ fn merge_pairs<G: CurveAffine>(v1: &[G], v2: &[G]) -> (G, G)
 
 /// This needs to be destroyed by at least one participant
 /// for the final parameters to be secure.
-pub struct PrivateKey {
+struct PrivateKey {
     delta: Fr
 }
 
 /// Compute a keypair, given the current parameters. Keypairs
 /// cannot be reused for multiple contributions or contributions
 /// in different parameters.
-pub fn keypair<R: Rng>(
+fn keypair<R: Rng>(
     rng: &mut R,
     current: &MPCParameters,
 ) -> (PublicKey, PrivateKey)
