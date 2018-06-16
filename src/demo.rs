763f8eac9682f90d742b5cb76e054a525e79d21a
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-06-16 22:19:35 +0200

    roundtrip some bytes

--- src/demo.rs
+++ src/demo.rs
@@ -3,6 +3,7 @@ mod tests {
     use pairing::bls12_381::*;
     use pairing::{PrimeField, PrimeFieldRepr};
     use rand::{Rng, SeedableRng, XorShiftRng};
+    use sapling_crypto::circuit::multipack;
 
     #[test]
     fn roundtrip_fr_bytes() {
@@ -37,6 +38,54 @@ mod tests {
 
             // it's the same
             assert_eq!(fr, fr_back);
+        }
+    }
+
+    #[test]
+    fn roundtrip_bytes() {
+        let rng = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+
+        for _ in 0..100 {
+            // lets say we have some random bytes
+            let input: Vec<u8> = (0..128).map(|_| rng.gen()).collect();
+
+            // lets convert them to bits
+            let input_bits: Vec<bool> = multipack::bytes_to_bits(&input);
+
+            // make sure we have the right amount of bits
+            assert_eq!(input_bits.len(), 128 * 8);
+
+            // which we want to store in Fr elements
+            let frs = multipack::compute_multipacking::<Bls12>(&input_bits);
+
+            // lets convert these into bytes that we could store somewhere
+            let bytes = frs.iter().fold(Vec::new(), |mut acc, fr| {
+                fr.into_repr().write_le(&mut acc).unwrap();
+                acc
+            });
+
+            assert_eq!(bytes.len(), frs.len() * 32);
+
+            // lets pull them back into frs
+            let frs_back: Vec<Fr> = bytes
+                .chunks(8 * 4)
+                .map(|chunk| {
+                    let mut fr_repr_back = FrRepr::default();
+                    fr_repr_back.read_le(chunk).unwrap();
+                    Fr::from_repr(fr_repr_back).unwrap()
+                })
+                .collect();
+
+            assert_eq!(frs.len(), frs_back.len());
+            assert_eq!(frs, frs_back);
+
+            // get our bytes back
+            let bytes_back: Vec<u8> = frs_back.iter().fold(Vec::new(), |mut acc, fr| {
+                fr.into_repr().write_le(&mut acc).unwrap();
+                acc
+            });
+
+            assert_eq!(bytes, bytes_back);
         }
     }
 }
