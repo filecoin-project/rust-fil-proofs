191eb2d9aafe142995474dc3d0be56255010cad2
nicola <nicola@users.noreply.github.com>
2018-06-18 04:14:54 -0700

    feat: add rounds to sloth

--- src/circuit/sloth.rs
+++ src/circuit/sloth.rs
@@ -2,54 +2,64 @@ use bellman::{ConstraintSystem, SynthesisError};
 use pairing::{Engine, Field};
 
 /// Circuit version of sloth decoding.
-pub fn decode<E, CS>(cs: &mut CS, k_value: E::Fr, c_value: E::Fr) -> Result<E::Fr, SynthesisError>
+pub fn decode<E, CS>(
+    mut cs: CS,
+    key: &E::Fr,
+    ciphertext: &E::Fr,
+    rounds: usize,
+) -> Result<E::Fr, SynthesisError>
 where
     E: Engine,
     CS: ConstraintSystem<E>,
 {
-    // Compute (c)^5-k mod p.
+    let mut plaintext = *ciphertext;
 
-    // c
-    let mut tmp_value = c_value;
-    let c = cs.alloc(|| "c", || Ok(tmp_value))?;
+    for i in 0..rounds {
+        let cs = &mut cs.namespace(|| format!("round {}", i));
 
-    // c^2
-    //    1  c c2 c4 out
-    // [  0  1  0  0  0 ] // a
-    // [  0  1  0  0  0 ] // b
-    // [  0  0  1  0  0 ] // c
-    // c1 * c1 = c2
-    tmp_value.square();
-    let c2 = cs.alloc(|| "c2", || Ok(tmp_value))?;
-    cs.enforce(|| "c2 = (c)^2", |lc| lc + c, |lc| lc + c, |lc| lc + c2);
+        // c
+        let mut tmp_value = plaintext;
+        let c = cs.alloc(|| "c", || Ok(tmp_value))?;
 
-    // c^4
-    //    1  c c2 c4 out
-    // [  0  0  1  0  0 ] // a
-    // [  0  0  1  0  0 ] // b
-    // [  0  0  0  1  0 ] // c
-    // c2 * c2 = c4
-    tmp_value.square();
-    let c4 = cs.alloc(|| "c4", || Ok(tmp_value))?;
-    cs.enforce(|| "c4 = (c2)^2", |lc| lc + c2, |lc| lc + c2, |lc| lc + c4);
+        // c^2
+        //    1  c c2 c4 out
+        // [  0  1  0  0  0 ] // a
+        // [  0  1  0  0  0 ] // b
+        // [  0  0  1  0  0 ] // c
+        // c1 * c1 = c2
+        tmp_value.square();
+        let c2 = cs.alloc(|| "c2", || Ok(tmp_value))?;
+        cs.enforce(|| "c2 = (c)^2", |lc| lc + c, |lc| lc + c, |lc| lc + c2);
 
-    // c^4*c - k
-    //    1  c c2 c4 out
-    // [  0  0  0  1  0 ] // a
-    // [  0  1  0  0  0 ] // b
-    // [  k  0  0  0  1 ] // c
-    // (c4)*(c) = out + k => c^4*c-k = out
-    tmp_value.mul_assign(&c_value);
-    tmp_value.sub_assign(&k_value);
-    let output = cs.alloc(|| "output", || Ok(tmp_value))?;
-    cs.enforce(
-        || "c5 = (c4)*c - k",
-        |lc| lc + c4,
-        |lc| lc + c,
-        |lc| lc + output + (k_value, CS::one()),
-    );
+        // c^4
+        //    1  c c2 c4 out
+        // [  0  0  1  0  0 ] // a
+        // [  0  0  1  0  0 ] // b
+        // [  0  0  0  1  0 ] // c
+        // c2 * c2 = c4
+        tmp_value.square();
+        let c4 = cs.alloc(|| "c4", || Ok(tmp_value))?;
+        cs.enforce(|| "c4 = (c2)^2", |lc| lc + c2, |lc| lc + c2, |lc| lc + c4);
 
-    Ok(tmp_value)
+        // c^4*c - k
+        //    1  c c2 c4 out
+        // [  0  0  0  1  0 ] // a
+        // [  0  1  0  0  0 ] // b
+        // [  k  0  0  0  1 ] // c
+        // (c4)*(c) = out + k => c^4*c-k = out
+        tmp_value.mul_assign(&plaintext);
+        tmp_value.sub_assign(key);
+        let output = cs.alloc(|| "output", || Ok(tmp_value))?;
+        cs.enforce(
+            || "c5 = (c4)*c - k",
+            |lc| lc + c4,
+            |lc| lc + c,
+            |lc| lc + output + (*key, CS::one()),
+        );
+        plaintext = tmp_value;
+    }
+
+    Ok(plaintext)
 }
 
 #[cfg(test)]
@@ -59,34 +69,70 @@ mod tests {
     use crypto::sloth;
     use pairing::bls12_381::{Bls12, Fr};
     use pairing::PrimeField;
+    use rand::{Rng, SeedableRng, XorShiftRng};
 
     #[test]
-    fn test_snark_sloth_dec() {
-        let key = Fr::from_str("11111111").unwrap();
-        let plaintext = Fr::from_str("123456789").unwrap();
-        let ciphertext = sloth::encode::<Bls12>(&key, &plaintext);
+    fn sloth_snark_decode() {
+        let rng = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
 
-        // Vanilla
-        let decrypted = sloth::decode::<Bls12>(&key, &ciphertext);
-        let mut cs = TestConstraintSystem::<Bls12>::new();
-        let out = decode(&mut cs, key, ciphertext).unwrap();
+        for _ in 0..10 {
+            let key: Fr = rng.gen();
+            let plaintext: Fr = rng.gen();
+            let ciphertext = sloth::encode::<Bls12>(&key, &plaintext, 10);
 
-        assert!(cs.is_satisfied());
-        assert_eq!(out, decrypted);
+            // Vanilla
+            let decrypted = sloth::decode::<Bls12>(&key, &ciphertext, 10);
+            let mut cs = TestConstraintSystem::<Bls12>::new();
+            let out = decode(cs.namespace(|| "sloth"), &key, &ciphertext, 10).unwrap();
+
+            assert!(cs.is_satisfied());
+            assert_eq!(out, decrypted);
+        }
     }
+
     #[test]
-    fn test_snark_sloth_dec_bad() {
-        let key = Fr::from_str("11111111").unwrap();
-        let key_bad = Fr::from_str("11111112").unwrap();
-        let plaintext = Fr::from_str("123456789").unwrap();
-        let ciphertext = sloth::encode::<Bls12>(&key, &plaintext);
+    fn sloth_snark_decode_bad() {
+        let rng = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
 
-        // Vanilla
-        let decrypted = sloth::decode::<Bls12>(&key, &ciphertext);
-        let mut cs = TestConstraintSystem::<Bls12>::new();
-        let out = decode(&mut cs, key_bad, ciphertext).unwrap();
+        for _ in 0..10 {
+            let key: Fr = rng.gen();
+            let key_bad: Fr = rng.gen();
+            let plaintext: Fr = rng.gen();
 
-        assert!(cs.is_satisfied());
-        assert_ne!(out, decrypted);
+            let ciphertext = sloth::encode::<Bls12>(&key, &plaintext, 10);
+
+            let decrypted = sloth::decode::<Bls12>(&key, &ciphertext, 10);
+            let mut cs = TestConstraintSystem::<Bls12>::new();
+            let out = decode(cs.namespace(|| "sloth"), &key_bad, &ciphertext, 10).unwrap();
+
+            assert!(cs.is_satisfied());
+            assert_ne!(out, decrypted);
+        }
+    }
+
+    #[test]
+    fn sloth_snark_decode_different_iterations() {
+        let rng = &mut XorShiftRng::from_seed([0x3dbe6259, 0x8d313d76, 0x3237db17, 0xe5bc0654]);
+
+        for _ in 0..10 {
+            let key: Fr = rng.gen();
+            let plaintext: Fr = rng.gen();
+
+            let ciphertext = sloth::encode::<Bls12>(&key, &plaintext, 10);
+
+            let decrypted = sloth::decode::<Bls12>(&key, &ciphertext, 10);
+
+            let mut cs = TestConstraintSystem::<Bls12>::new();
+            let out9 = decode(cs.namespace(|| "sloth 9"), &key, &ciphertext, 9).unwrap();
+            let mut cs = TestConstraintSystem::<Bls12>::new();
+            let out10 = decode(cs.namespace(|| "sloth 10"), &key, &ciphertext, 10).unwrap();
+            let mut cs = TestConstraintSystem::<Bls12>::new();
+            let out11 = decode(cs.namespace(|| "sloth 11"), &key, &ciphertext, 11).unwrap();
+
+            assert!(cs.is_satisfied());
+            assert_ne!(out9, decrypted);
+            assert_eq!(out10, decrypted);
+            assert_ne!(out11, decrypted);
+        }
     }
 }
--- src/crypto/sloth.rs
+++ src/crypto/sloth.rs
@@ -1,26 +1,39 @@
 use pairing::{Engine, Field};
 
-// is the same as in `Fr::from_str("20974350070050476191779096203274386335076221000211055129041463479975432473805").unwrap().into_repr()`
+/// The `v` constant for sloth.
+/// This is the same as in `Fr::from_str("20974350070050476191779096203274386335076221000211055129041463479975432473805").unwrap().into_repr()`.
 const SLOTH_V: [u64; 4] = [
-    3689348813023923405,
-    2413663763415232921,
-    16233882818423549954,
-    3341406743785779740,
+    3_689_348_813_023_923_405,
+    2_413_663_763_415_232_921,
+    16_233_882_818_423_549_954,
+    3_341_406_743_785_779_740,
 ];
 
+/// The number five, as an array so we can use it in `pow`.
+const FIVE: [u64; 1] = [5];
+
 /// Sloth based encoding.
-pub fn encode<E: Engine>(key: &E::Fr, plaintext: &E::Fr) -> E::Fr {
-    let mut tmp = plaintext.clone();
-    tmp.add_assign(key); // c + k
-    tmp.pow(&SLOTH_V) // (c + k)^v
+pub fn encode<E: Engine>(key: &E::Fr, plaintext: &E::Fr, rounds: usize) -> E::Fr {
+    let mut ciphertext = *plaintext;
+
+    for _ in 0..rounds {
+        ciphertext.add_assign(key); // c + k
+        ciphertext = ciphertext.pow(&SLOTH_V) // (c + k)^v
+    }
+
+    ciphertext
 }
 
-/// Sloth based decoding
-pub fn decode<E: Engine>(key: &E::Fr, ciphertext: &E::Fr) -> E::Fr {
-    let mut tmp = ciphertext.pow(&[5]); // c^5
-    tmp.sub_assign(key); // c^5 - k
+/// Sloth based decoding.
+pub fn decode<E: Engine>(key: &E::Fr, ciphertext: &E::Fr, rounds: usize) -> E::Fr {
+    let mut plaintext = *ciphertext;
 
-    tmp
+    for _ in 0..rounds {
+        plaintext = plaintext.pow(&FIVE); // c^5
+        plaintext.sub_assign(key); // c^5 - k
+    }
+
+    plaintext
 }
 
 #[cfg(test)]
@@ -29,6 +42,7 @@ mod tests {
     use pairing::bls12_381::{Bls12, Fr, FrRepr};
     use pairing::PrimeField;
 
+    // the modulus from `bls12_381::Fr`
     const MODULUS: [u64; 4] = [
         0xffffffff00000001,
         0x53bda402fffe5bfe,
@@ -37,22 +51,22 @@ mod tests {
     ];
 
     #[test]
-    fn test_sloth_bls_12() {
+    fn sloth_bls_12() {
         let key = Fr::from_str("11111111").unwrap();
         let plaintext = Fr::from_str("123456789").unwrap();
-        let ciphertext = encode::<Bls12>(&key, &plaintext);
-        let decrypted = decode::<Bls12>(&key, &ciphertext);
+        let ciphertext = encode::<Bls12>(&key, &plaintext, 10);
+        let decrypted = decode::<Bls12>(&key, &ciphertext, 10);
         assert_eq!(plaintext, decrypted);
         assert_ne!(plaintext, ciphertext);
     }
 
     #[test]
-    fn test_sloth_bls_12_fake() {
+    fn sloth_bls_12_fake() {
         let key = Fr::from_str("11111111").unwrap();
         let key_fake = Fr::from_str("11111112").unwrap();
         let plaintext = Fr::from_str("123456789").unwrap();
-        let ciphertext = encode::<Bls12>(&key, &plaintext);
-        let decrypted = decode::<Bls12>(&key_fake, &ciphertext);
+        let ciphertext = encode::<Bls12>(&key, &plaintext, 10);
+        let decrypted = decode::<Bls12>(&key_fake, &ciphertext, 10);
         assert_ne!(plaintext, decrypted);
     }
 
@@ -64,9 +78,9 @@ mod tests {
 
     proptest!{
         #[test]
-        fn sloth_roundtrip(key in arb_fr(), plaintext in arb_fr()) {
-            let ciphertext = encode::<Bls12>(&key, &plaintext);
-            assert_eq!(decode::<Bls12>(&key, &ciphertext), plaintext);
+        fn sloth_bls_roundtrip(key in arb_fr(), plaintext in arb_fr()) {
+            let ciphertext = encode::<Bls12>(&key, &plaintext, 10);
+            assert_eq!(decode::<Bls12>(&key, &ciphertext, 10), plaintext);
         }
     }
 }
