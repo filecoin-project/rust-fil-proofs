09d1d369cd868e611f5587a4245921fd1eab6bd2
dignifiedquire <dignifiedquire@users.noreply.github.com>
2018-11-28 17:56:32 +0100

    update bacon post
    - no more initial challenges
    - concat t in challenge derivation
    - keep some state and simulate waiting for beacon output

--- storage-proofs/src/bacon_post.rs
+++ storage-proofs/src/bacon_post.rs
@@ -1,4 +1,5 @@
 use std::marker::PhantomData;
+use std::{thread, time};
 
 use byteorder::{ByteOrder, LittleEndian};
 
@@ -25,8 +26,6 @@ pub struct PublicParams<T: Domain, V: Vdf<T>> {
 pub struct PublicInputs<T: Domain> {
     /// The root hashes of the merkle trees of the sealed sectors.
     pub comm_rs: Vec<T>,
-    /// The initial set of challengs. Must be of length `challenge_count`.
-    pub challenges: Vec<T>,
 }
 
 #[derive(Clone, Debug)]
@@ -46,27 +45,53 @@ pub struct Proof<'a, H: Hasher + 'a, V: Vdf<H::Domain>>(Vec<hvh_post::Proof<'a, 
 pub struct BaconPost<H: Hasher, V: Vdf<H::Domain>> {
     _t: PhantomData<H>,
     _v: PhantomData<V>,
+    beacon: Beacon,
 }
 
-impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> ProofScheme<'a> for BaconPost<H, V> {
-    type PublicParams = PublicParams<H::Domain, V>;
-    type SetupParams = SetupParams<H::Domain, V>;
-    type PublicInputs = PublicInputs<H::Domain>;
-    type PrivateInputs = PrivateInputs<'a, H>;
-    type Proof = Proof<'a, H, V>;
+#[derive(Clone, Debug, Default)]
+struct Beacon {
+    count: usize,
+}
+impl<H: Hasher, V: Vdf<H::Domain>> Default for BaconPost<H, V> {
+    fn default() -> Self {
+        BaconPost {
+            _t: PhantomData,
+            _v: PhantomData,
+            beacon: Default::default(),
+        }
+    }
+}
 
-    fn setup(sp: &Self::SetupParams) -> Result<Self::PublicParams> {
+impl Beacon {
+    pub fn get<T: Domain>(&mut self, t: usize) -> T {
+        // TODO: actual beacon
+
+        if self.count < t {
+            // sleep a bit, to simulate dely
+            thread::sleep(time::Duration::from_millis(10));
+            self.count += 1;
+        }
+
+        let mut bytes = [0u8; 32];
+        LittleEndian::write_u32(&mut bytes, t as u32);
+        T::try_from_bytes(&bytes).expect("invalid beacon element")
+    }
+}
+
+impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> BaconPost<H, V> {
+    pub fn setup(&self, sp: &SetupParams<H::Domain, V>) -> Result<PublicParams<H::Domain, V>> {
         Ok(PublicParams {
             pub_params_hvh_post: hvh_post::HvhPost::<H, V>::setup(&sp.setup_params_hvh_post)?,
             post_periods_count: sp.post_periods_count,
         })
     }
 
-    fn prove<'b>(
-        pub_params: &'b Self::PublicParams,
-        pub_inputs: &'b Self::PublicInputs,
-        priv_inputs: &'b Self::PrivateInputs,
-    ) -> Result<Self::Proof> {
+    pub fn prove<'b>(
+        &mut self,
+        pub_params: &'b PublicParams<H::Domain, V>,
+        pub_inputs: &'b PublicInputs<H::Domain>,
+        priv_inputs: &'b PrivateInputs<'a, H>,
+    ) -> Result<Proof<'a, H, V>> {
         let sectors_count = pub_params.pub_params_hvh_post.sectors_count;
         let challenge_count = pub_params.pub_params_hvh_post.challenge_count;
         let post_periods_count = pub_params.post_periods_count;
@@ -83,14 +108,28 @@ impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> ProofScheme<'a> for BaconPost<H, V> 
 
         // First (t = 0)
         {
+            // Run Bacon
+            let r = self.beacon.get::<H::Domain>(0);
+
+            // Generate challenges
+            let challenges: Vec<H::Domain> = (0..challenge_count)
+                .map(|i| {
+                    let mut i_bytes = [0u8; 32];
+                    LittleEndian::write_u32(&mut i_bytes[0..4], 0u32);
+                    LittleEndian::write_u32(&mut i_bytes[4..8], i as u32);
+
+                    H::Function::hash(&[r.as_ref(), &i_bytes].concat())
+                })
+                .collect();
+
             // TODO: avoid cloining
             let pub_inputs_hvh_post = hvh_post::PublicInputs {
                 comm_rs: pub_inputs.comm_rs.clone(),
-                challenges: pub_inputs.challenges.clone(),
+                challenges,
             };
 
             let priv_inputs_hvh_post =
-                hvh_post::PrivateInputs::new(priv_inputs.replicas, priv_inputs.trees);
+                hvh_post::PrivateInputs::<'a, H>::new(priv_inputs.replicas, priv_inputs.trees);
 
             proofs_hvh_post.push(hvh_post::HvhPost::prove(
                 &pub_params.pub_params_hvh_post,
@@ -100,17 +139,17 @@ impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> ProofScheme<'a> for BaconPost<H, V> 
         }
 
         // The rest (t = 1..post_periods_count)
-
         for t in 1..post_periods_count {
             // Run Bacon
-            let r = random_beacon::<H::Domain>(t);
+            let r = self.beacon.get::<H::Domain>(t);
             let x = extract_post_input::<H, V>(&proofs_hvh_post[t - 1]);
 
             // Generate challenges
             let challenges: Vec<H::Domain> = (0..challenge_count)
                 .map(|i| {
                     let mut i_bytes = [0u8; 32];
-                    LittleEndian::write_u32(&mut i_bytes[0..4], i as u32);
+                    LittleEndian::write_u32(&mut i_bytes[0..4], t as u32);
+                    LittleEndian::write_u32(&mut i_bytes[4..8], i as u32);
 
                     H::Function::hash(&[x.as_ref(), r.as_ref(), &i_bytes].concat())
                 })
@@ -136,10 +175,11 @@ impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> ProofScheme<'a> for BaconPost<H, V> 
         Ok(Proof(proofs_hvh_post))
     }
 
-    fn verify(
-        pub_params: &Self::PublicParams,
-        pub_inputs: &Self::PublicInputs,
-        proof: &Self::Proof,
+    pub fn verify(
+        &mut self,
+        pub_params: &PublicParams<H::Domain, V>,
+        pub_inputs: &PublicInputs<H::Domain>,
+        proof: &Proof<H, V>,
     ) -> Result<bool> {
         let challenge_count = pub_params.pub_params_hvh_post.challenge_count;
         let post_periods_count = pub_params.post_periods_count;
@@ -148,9 +188,21 @@ impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> ProofScheme<'a> for BaconPost<H, V> 
 
         // First (t = 0)
         {
+            let r = self.beacon.get::<H::Domain>(0);
+            // Generate challenges
+            let challenges: Vec<H::Domain> = (0..challenge_count)
+                .map(|i| {
+                    let mut i_bytes = [0u8; 32];
+                    LittleEndian::write_u32(&mut i_bytes[0..4], 0u32);
+                    LittleEndian::write_u32(&mut i_bytes[4..8], i as u32);
+
+                    H::Function::hash(&[r.as_ref(), &i_bytes].concat())
+                })
+                .collect();
+
             // TODO: avoid cloining
             let pub_inputs_hvh_post = hvh_post::PublicInputs {
-                challenges: pub_inputs.challenges.clone(),
+                challenges,
                 comm_rs: pub_inputs.comm_rs.clone(),
             };
 
@@ -166,14 +218,14 @@ impl<'a, H: Hasher + 'a, V: Vdf<H::Domain>> ProofScheme<'a> for BaconPost<H, V> 
         // The rest (t = 1..post_periods_count)
         for t in 1..post_periods_count {
             // Generate challenges
-
-            let r = random_beacon::<H::Domain>(t);
+            let r = self.beacon.get::<H::Domain>(t);
             let x = extract_post_input::<H, V>(&proof.0[t - 1]);
 
             let challenges: Vec<H::Domain> = (0..challenge_count)
                 .map(|i| {
                     let mut i_bytes = [0u8; 32];
-                    LittleEndian::write_u32(&mut i_bytes[0..4], i as u32);
+                    LittleEndian::write_u32(&mut i_bytes[0..4], t as u32);
+                    LittleEndian::write_u32(&mut i_bytes[4..8], i as u32);
 
                     H::Function::hash(&[x.as_ref(), r.as_ref(), &i_bytes].concat())
                 })
@@ -213,14 +265,6 @@ fn extract_post_input<H: Hasher, V: Vdf<H::Domain>>(proof: &hvh_post::Proof<H, V
     H::Function::hash(&leafs)
 }
 
-fn random_beacon<T: Domain>(t: usize) -> T {
-    // TODO: actual beacon
-
-    let mut bytes = [0u8; 32];
-    LittleEndian::write_u32(&mut bytes, t as u32);
-    T::try_from_bytes(&bytes).expect("invalid beacon element")
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -253,7 +297,9 @@ mod tests {
             post_periods_count: 3,
         };
 
-        let pub_params = BaconPost::<PedersenHasher, vdf_sloth::Sloth>::setup(&sp).unwrap();
+        let mut bacon_post = BaconPost::<PedersenHasher, vdf_sloth::Sloth>::default();
+
+        let pub_params = bacon_post.setup(&sp).unwrap();
 
         let data0: Vec<u8> = (0..1024)
             .flat_map(|_| fr_into_bytes::<Bls12>(&rng.gen()))
@@ -268,7 +314,6 @@ mod tests {
         let tree1 = graph1.merkle_tree(data1.as_slice(), lambda).unwrap();
 
         let pub_inputs = PublicInputs {
-            challenges: vec![rng.gen(), rng.gen()],
             comm_rs: vec![tree0.root(), tree1.root()],
         };
 
@@ -278,16 +323,10 @@ mod tests {
             _h: PhantomData,
         };
 
-        let proof = BaconPost::<PedersenHasher, vdf_sloth::Sloth>::prove(
-            &pub_params,
-            &pub_inputs,
-            &priv_inputs,
-        )
-        .unwrap();
+        let proof = bacon_post
+            .prove(&pub_params, &pub_inputs, &priv_inputs)
+            .unwrap();
 
-        assert!(
-            BaconPost::<PedersenHasher, vdf_sloth::Sloth>::verify(&pub_params, &pub_inputs, &proof)
-                .unwrap()
-        );
+        assert!(bacon_post.verify(&pub_params, &pub_inputs, &proof).unwrap());
     }
 }
