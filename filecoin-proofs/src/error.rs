27de98703ab0f42bca524fbe2162528cbcfe732d
Sidney Keese <sidney@carbonfive.com>
2018-12-12 09:43:37 -0800

    ErrorLogResult and swap out expects/unwraps for loggable calls

--- filecoin-proofs/src/error.rs
+++ filecoin-proofs/src/error.rs
@@ -1,3 +1,30 @@
-use failure::Error;
+use crate::FCP_LOG;
+use failure::{Backtrace, Error};
+use slog::*;
 
 pub type Result<T> = ::std::result::Result<T, Error>;
+
+pub trait ErrorLogResult<T> {
+    fn expect_with_logging(self, msg: &str) -> T;
+}
+
+impl<T, E: std::fmt::Debug> ErrorLogResult<T> for ::std::result::Result<T, E> {
+    fn expect_with_logging(self, msg: &str) -> T {
+        if let Err(ref err) = self {
+            let err = format!("{:?}", err);
+            let backtrace = format!("{:?}", Backtrace::new());
+            error!(FCP_LOG, "expected Result to be Ok"; "error" => err, "backtrace" => backtrace);
+        }
+        self.expect(msg)
+    }
+}
+
+impl<T> ErrorLogResult<T> for Option<T> {
+    fn expect_with_logging(self, msg: &str) -> T {
+        if self.is_none() {
+            let backtrace = format!("{:?}", Backtrace::new());
+            error!(FCP_LOG, "expected Option to be Some"; "backtrace" => backtrace);
+        }
+        self.expect(msg)
+    }
+}
