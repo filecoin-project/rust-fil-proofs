312cdc12ea424940d8818a220f384d884a3aba17
Sidney Keese <sidney@carbonfive.com>
2018-12-20 14:04:46 -0800

    maybe this location flag works on ci

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -31,7 +31,7 @@ echo "release file created: $RELEASE_FILE"
 # see if the release already exists by tag
 RELEASE_RESPONSE=`
   curl \
-    --verbose \
+    --location \
     --header "Authorization: token $GITHUB_TOKEN" \
     "https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/releases/tags/$RELEASE_TAG"
 `
@@ -51,7 +51,7 @@ if [ "$RELEASE_ID" = "null" ]; then
   # create it if it doesn't exist yet
   RELEASE_RESPONSE=`
     curl \
-      --verbose \
+      --location \
       --request POST \
       --header "Authorization: token $GITHUB_TOKEN" \
       --header "Content-Type: application/json" \
@@ -68,6 +68,7 @@ echo $RELEASE_RESPONSE
 RELEASE_UPLOAD_URL=`echo $RELEASE_RESPONSE | jq -r '.upload_url' | cut -d'{' -f1`
 
 curl \
+  --location \
   --request POST \
   --header "Authorization: token $GITHUB_TOKEN" \
   --header "Content-Type: application/octet-stream" \
