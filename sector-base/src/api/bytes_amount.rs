c8597509dbd69f31215ecf2e2d5f2d5770ddbc4c
porcuquine <porcuquine@users.noreply.github.com>
2019-02-25 13:42:10 -0800

    Add operation test to document behavior.

--- sector-base/src/api/bytes_amount.rs
+++ sector-base/src/api/bytes_amount.rs
@@ -62,3 +62,46 @@ impl Sub for PaddedBytesAmount {
         PaddedBytesAmount(self.0 - other.0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn allowed_operations() {
+        let a = UnpaddedBytesAmount(1);
+        let b = UnpaddedBytesAmount(2);
+        let c = UnpaddedBytesAmount(3);
+
+        let d = PaddedBytesAmount(1);
+        let e = PaddedBytesAmount(2);
+        let f = PaddedBytesAmount(3);
+
+        // Operations between UnpaddedBytesAmounts are allowed
+        assert_eq!(a + b, c);
+        assert_eq!(c - b, a);
+
+        // Operations between PaddedBytesAmounts are allowed
+        assert_eq!(d + e, f);
+        assert_eq!(f - e, d);
+
+        // Mixed operations fail at compile time.
+        // assert_eq!(a + b, f);
+
+        // Coercion to primitives work
+        assert_eq!(1u64 + u64::from(b), 3u64);
+        assert_eq!(1usize + usize::from(b), 3usize);
+        assert_eq!(1u64 + u64::from(e), 3u64);
+        assert_eq!(1usize + usize::from(e), 3usize);
+
+        // But not between BytesAmount types
+        // assert_eq!(a + UnpaddedBytesAmount::from(e), c);
+        // assert_eq!(d + UnpaddedBytesAmount::from(b), f);
+
+        // But must be explicit or won't compile.
+        // assert_eq!(1u64 + b, 3u64);
+        // assert_eq!(1usize + b, 3usize);
+        // assert_eq!(1u64 + u64::from(e), 3u64);
+        // assert_eq!(1usize + usize::from(e), 3usize);
+    }
+}
