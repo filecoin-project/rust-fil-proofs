59688e9b9f2fe08e52e19d1c6798d20a5efc67a7
laser <l@s3r.me>
2019-02-26 09:55:43 -0800

    modify paramcache to optionally generate PoSt parameters

--- filecoin-proofs/src/bin/paramcache.rs
+++ filecoin-proofs/src/bin/paramcache.rs
@@ -5,7 +5,6 @@ extern crate storage_proofs;
 
 use filecoin_proofs::api::internal;
 use pairing::bls12_381::Bls12;
-
 use sector_base::api::bytes_amount::PaddedBytesAmount;
 use sector_base::api::disk_backed_storage::{LIVE_SECTOR_SIZE, TEST_SECTOR_SIZE};
 use storage_proofs::circuit::vdf_post::{VDFPoStCircuit, VDFPostCompound};
@@ -16,9 +15,9 @@ use storage_proofs::parameter_cache::CacheableParameters;
 use storage_proofs::vdf_post::VDFPoSt;
 use storage_proofs::vdf_sloth::Sloth;
 
-const GENERATE_POST_PARAMS: bool = false;
+use std::env;
 
-fn cache_params(sector_size: u64) {
+fn cache_porep_params(sector_size: u64) {
     let bytes_amount = PaddedBytesAmount(sector_size);
 
     let public_params = internal::public_params(bytes_amount);
@@ -31,33 +30,42 @@ fn cache_params(sector_size: u64) {
         let circuit = ZigZagCompound::blank_circuit(&public_params, &internal::ENGINE_PARAMS);
         let _ = ZigZagCompound::get_verifying_key(circuit, &public_params);
     }
+}
 
-    if GENERATE_POST_PARAMS {
-        let post_public_params = internal::post_public_params(bytes_amount);
-        {
-            let post_circuit: VDFPoStCircuit<Bls12> =
-                <VDFPostCompound as CompoundProof<
-                    Bls12,
-                    VDFPoSt<PedersenHasher, Sloth>,
-                    VDFPoStCircuit<Bls12>,
-                >>::blank_circuit(&post_public_params, &internal::ENGINE_PARAMS);
-            let _ = VDFPostCompound::get_groth_params(post_circuit, &post_public_params);
-        }
-        {
-            let post_circuit: VDFPoStCircuit<Bls12> =
-                <VDFPostCompound as CompoundProof<
-                    Bls12,
-                    VDFPoSt<PedersenHasher, Sloth>,
-                    VDFPoStCircuit<Bls12>,
-                >>::blank_circuit(&post_public_params, &internal::ENGINE_PARAMS);
+fn cache_post_params(sector_size: u64) {
+    let bytes_amount = PaddedBytesAmount(sector_size);
 
-            let _ = VDFPostCompound::get_verifying_key(post_circuit, &post_public_params);
-        }
+    let post_public_params = internal::post_public_params(bytes_amount);
+    {
+        let post_circuit: VDFPoStCircuit<Bls12> =
+            <VDFPostCompound as CompoundProof<
+                Bls12,
+                VDFPoSt<PedersenHasher, Sloth>,
+                VDFPoStCircuit<Bls12>,
+            >>::blank_circuit(&post_public_params, &internal::ENGINE_PARAMS);
+        let _ = VDFPostCompound::get_groth_params(post_circuit, &post_public_params);
+    }
+    {
+        let post_circuit: VDFPoStCircuit<Bls12> =
+            <VDFPostCompound as CompoundProof<
+                Bls12,
+                VDFPoSt<PedersenHasher, Sloth>,
+                VDFPoStCircuit<Bls12>,
+            >>::blank_circuit(&post_public_params, &internal::ENGINE_PARAMS);
+
+        let _ = VDFPostCompound::get_verifying_key(post_circuit, &post_public_params);
     }
 }
 
 // Run this from the command-line to pre-generate the groth parameters used by the API.
 pub fn main() {
-    cache_params(TEST_SECTOR_SIZE);
-    cache_params(LIVE_SECTOR_SIZE);
+
+    // We can delete this guard once rust-fil-proofs issue #510 lands.
+    if let Some("true") = env::args().collect().get(1).map(|x| x.as_ref()) {
+        cache_post_params(TEST_SECTOR_SIZE);
+        cache_post_params(LIVE_SECTOR_SIZE);
+    }
+
+    cache_porep_params(TEST_SECTOR_SIZE);
+    cache_porep_params(LIVE_SECTOR_SIZE);
 }
