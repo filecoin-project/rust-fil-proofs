1ba91649f9a8e7a854af032b5a0f1aca74be0f3c
Sidney Keese <sidney@carbonfive.com>
2019-03-07 16:55:05 -0800

    initialize config with env overrides

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -7,30 +7,55 @@ extern crate toml;
 use failure::{Error, err_msg};
 use std::collections::HashMap;
 use std::fs::File;
+use std::path::Path;
 use std::io::prelude::Read;
 use std::sync::Mutex;
+use std::env;
 
 type Result<T> = ::std::result::Result<T, Error>;
 type Config = HashMap<String, bool>;
 
+const DEFAULT_CONFIG_PATH: &str = "./rust-fil-proofs.default.config.toml";
 const CONFIG_PATH: &str = "./rust-fil-proofs.config.toml";
 
 lazy_static! {
-    pub static ref CONFIG: Mutex<Config> = Mutex::new(load_config_toml(CONFIG_PATH).unwrap());
+    pub static ref CONFIG: Mutex<Config> = Mutex::new(initialize_config().unwrap());
 }
 
-pub fn load_config_toml(path: &str) -> Result<Config> {
-    let mut f = File::open(path)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
+pub fn initialize_config() -> Result<Config> {
+    let default_config: Config = load_config_from_toml(DEFAULT_CONFIG_PATH)?;
+    let toml_config: Config = load_config_from_toml(CONFIG_PATH)?;
+    let env_config: Config = load_config_from_env(&default_config)?;
 
-    let config: HashMap<String, bool> = toml::from_str(&contents)?;
+    Ok(default_config.into_iter().chain(toml_config).chain(env_config).collect())
+}
 
-    for (key, &value) in config.iter() {
-        set_config(key, value)?;
+pub fn load_config_from_toml(path: &str) -> Result<Config> {
+    let path = Path::new(path);
+
+    if path.exists() {
+        let mut f = File::open(path)?;
+        let mut contents = String::new();
+        f.read_to_string(&mut contents)?;
+
+        Ok(toml::from_str(&contents)?)
+    } else {
+        Ok(Default::default())
+    }
+}
+
+pub fn load_config_from_env(config: &Config) -> Result<Config> {
+    let mut env_config: Config = Default::default();
+
+    for key in config.keys() {
+        let var = env::var(&key);
+
+        if var.is_ok() {
+            env_config.insert(key.to_string(), var.unwrap() != "");
+        }
     }
 
-    Ok(config)
+    Ok(env_config)
 }
 
 pub fn set_config(key: &str, value: bool) -> Result<()> {
@@ -41,10 +66,18 @@ pub fn set_config(key: &str, value: bool) -> Result<()> {
 }
 
 pub fn get_config(key: &str) -> Result<bool> {
-    let config = &mut (*CONFIG).lock().unwrap();
+    let config = (*CONFIG).lock().unwrap();
 
     match config.get(key) {
         Some(&value) => Ok(value),
         None => Err(err_msg("key not found in config")),
+    }
+}
+
+pub fn debug_config() {
+    let config = (*CONFIG).lock().unwrap();
+
+    for (key, value) in config.iter() {
+        println!("{}: {}", key, value);
     }
 }
