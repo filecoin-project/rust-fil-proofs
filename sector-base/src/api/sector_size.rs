43311dbb537babab96ddcabeee928b93a8600472
laser <l@s3r.me>
2019-04-30 07:32:21 -0700

    drop unused const

--- sector-base/src/api/sector_size.rs
+++ sector-base/src/api/sector_size.rs
@@ -1,14 +1,9 @@
 use crate::api::bytes_amount::PaddedBytesAmount;
 use crate::api::bytes_amount::UnpaddedBytesAmount;
-use crate::api::disk_backed_storage::LIVE_SECTOR_SIZE;
-use crate::api::disk_backed_storage::TEST_SECTOR_SIZE;
 use crate::io::fr32::unpadded_bytes;
 
 #[derive(Clone, Copy, Debug)]
 pub struct SectorSize(pub u64);
-
-pub const SECTOR_SIZE_CHOICES: [SectorSize; 2] =
-    [SectorSize(TEST_SECTOR_SIZE), SectorSize(LIVE_SECTOR_SIZE)];
 
 impl From<SectorSize> for UnpaddedBytesAmount {
     fn from(x: SectorSize) -> Self {
