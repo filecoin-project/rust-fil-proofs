db2b1f00e21fc3df88485f14d0eb2a20890066f5
laser <l@s3r.me>
2019-05-06 11:57:55 -0700

    terminate unused impl

--- sector-base/src/api/porep_proof_partitions.rs
+++ sector-base/src/api/porep_proof_partitions.rs
@@ -1,4 +1,3 @@
-use crate::api::bytes_amount::PoRepProofBytesAmount;
 use crate::api::SINGLE_PARTITION_PROOF_LEN;
 
 #[derive(Clone, Copy, Debug)]
@@ -7,12 +6,6 @@ pub struct PoRepProofPartitions(pub u8);
 impl From<PoRepProofPartitions> for usize {
     fn from(x: PoRepProofPartitions) -> Self {
         x.0 as usize
-    }
-}
-
-impl From<PoRepProofPartitions> for PoRepProofBytesAmount {
-    fn from(x: PoRepProofPartitions) -> Self {
-        PoRepProofBytesAmount(usize::from(x) * SINGLE_PARTITION_PROOF_LEN)
     }
 }
 
