bf3de0ef735754dff03d67e7500fedc5d5c70d2c
laser <l@s3r.me>
2019-06-10 13:25:58 -0700

    we aren't packaging FFI-related code into this release any longer

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -12,16 +12,11 @@ TAR_PATH=`mktemp -d`
 
 mkdir -p $TAR_PATH
 mkdir -p $TAR_PATH/bin
-mkdir -p $TAR_PATH/include
-mkdir -p $TAR_PATH/lib/pkgconfig
 mkdir -p $TAR_PATH/misc
 
 cp parameters.json $TAR_PATH/misc/
 cp target/release/paramcache $TAR_PATH/bin/
 cp target/release/paramfetch $TAR_PATH/bin/
-cp target/release/libsector_builder_ffi.h $TAR_PATH/include/
-cp target/release/libsector_builder_ffi.a $TAR_PATH/lib/
-cp target/release/libsector_builder_ffi.pc $TAR_PATH/lib/pkgconfig
 
 pushd $TAR_PATH
 
