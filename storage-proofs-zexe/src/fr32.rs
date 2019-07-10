bf26620149b6f27fc4a1d89e31238dd3937efa54
YUBI <stefandeml@gmail.com>
2019-07-10 11:21:13 +0200

    fixing some other tests

--- storage-proofs-zexe/src/fr32.rs
+++ storage-proofs-zexe/src/fr32.rs
@@ -101,19 +101,24 @@ pub fn u32_into_fr<E: Engine>(n: u32) -> E::Fr {
 #[cfg(test)]
 mod tests {
     use super::*;
-    // use paired::bls12_381::Bls12;
     use algebra::curves::bls12_381::Bls12_381 as Bls12;
 
-    fn bytes_fr_test<E: Engine>(bytes: Fr32Ary, expect_success: bool) {
+    // Note: zexe returns the zero field element for bigint representations that overflow / are not in the field,
+    // while bellman was throwing an error. Due to this we had to adapt the logic of the following tests.
+
+    fn bytes_fr_test<E: Engine>(bytes: Fr32Ary, expect_zero: bool) {
         let mut b = &bytes[..];
         let fr_result = bytes_into_fr::<E>(&mut b);
-        if expect_success {
+        if expect_zero {
+
+            assert_eq!(fr_result.unwrap(), E::Fr::zero())
+
+        } else {
+
             let f = fr_result.expect("Failed to convert bytes to `Fr`");
             let b2 = fr_into_bytes::<E>(&f);
 
             assert_eq!(bytes.to_vec(), b2);
-        } else {
-            assert!(fr_result.is_err(), "expected a decoding error")
         }
     }
     #[test]
@@ -123,7 +128,7 @@ mod tests {
                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
                 23, 24, 25, 26, 27, 28, 29, 30, 31,
             ],
-            true,
+            false,
         );
         bytes_fr_test::<Bls12>(
             // Some bytes fail because they are not in the field.
@@ -131,7 +136,7 @@ mod tests {
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 115,
             ],
-            false,
+            true,
         );
         bytes_fr_test::<Bls12>(
             // This is okay.
@@ -139,7 +144,7 @@ mod tests {
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 114,
             ],
-            true,
+            false,
         );
         bytes_fr_test::<Bls12>(
             // So is this.
@@ -147,7 +152,7 @@ mod tests {
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 236, 115,
             ],
-            true,
+            false,
         );
         bytes_fr_test::<Bls12>(
             // But not this.
@@ -155,7 +160,7 @@ mod tests {
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
                 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 237, 115,
             ],
-            false,
+            true,
         );
     }
 
