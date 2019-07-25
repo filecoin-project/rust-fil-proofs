fa4bf8633109a91458d0c12bc9858899fa9f1bd2
Valentin Ganev <sanchopansa@gmail.com>
2019-07-25 14:34:32 +0300

    remove unused file

--- storage-proofs/src/hasher/mod.rs
+++ storage-proofs/src/hasher/mod.rs
@@ -4,11 +4,9 @@ pub mod sha256;
 
 mod digest;
 mod types;
-mod types_zexe;
 
 pub use self::digest::{DigestDomain, DigestFunction, DigestHasher, Digester};
 pub use self::types::{Domain, HashFunction, Hasher};
-pub use self::types_zexe::{DomainZexe, HashFunctionZexe, HasherZexe};
 
 pub use self::blake2s::Blake2sHasher;
 pub use self::pedersen::PedersenHasher;
