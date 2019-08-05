85c6ed6037b181023ac0eff0aeb94f0f9edd2ad4
laser <l@s3r.me>
2019-08-05 16:54:12 -0700

    fixup(run special magic branch)

--- fil-proofs-tooling/scripts/benchy-remote.sh
+++ fil-proofs-tooling/scripts/benchy-remote.sh
@@ -6,7 +6,7 @@ CMDS=$(cat <<EOF
 cd \$(mktemp -d)
 git clone -q https://github.com/filecoin-project/rust-fil-proofs.git
 cd rust-fil-proofs
-git checkout -q master
+git checkout -q feat/761-remote-benchy
 RUSTFLAGS="-Awarnings -C target-cpu=native" ./fil-proofs-tooling/scripts/benchy.sh ${@:2}
 EOF
 )
--- fil-proofs-tooling/scripts/micro-remote.sh
+++ fil-proofs-tooling/scripts/micro-remote.sh
@@ -6,7 +6,7 @@ CMDS=$(cat <<EOF
 cd \$(mktemp -d)
 git clone -q https://github.com/filecoin-project/rust-fil-proofs.git
 cd rust-fil-proofs
-git checkout -q master
+git checkout -q feat/761-remote-benchy
 RUSTFLAGS="-Awarnings -C target-cpu=native" /root/.cargo/bin/cargo run --bin micro --release ${@:2}
 EOF
 )
