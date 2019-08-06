5fccbf8f530dc2059c825eef4a5e99c045c59bc4
laser <l@s3r.me>
2019-08-05 17:47:35 -0700

    fixup(revert to master)

--- fil-proofs-tooling/scripts/benchy-remote.sh
+++ fil-proofs-tooling/scripts/benchy-remote.sh
@@ -6,7 +6,7 @@ CMDS=$(cat <<EOF
 cd \$(mktemp -d)
 git clone -q https://github.com/filecoin-project/rust-fil-proofs.git
 cd rust-fil-proofs
-git checkout -q feat/761-remote-benchy
+git checkout -q master
 ./fil-proofs-tooling/scripts/benchy.sh ${@:2}
 EOF
 )
