ad9990035b8d4611722a958c85993867a8ffa4b2
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-08-10 01:06:21 +0200

    chore(clippy): allow type repetition in bounds to deal with serde

--- storage-proofs/src/lib.rs
+++ storage-proofs/src/lib.rs
@@ -1,6 +1,5 @@
 #![deny(clippy::all, clippy::perf, clippy::correctness)]
-#![allow(clippy::unreadable_literal)]
-#![allow(clippy::type_repetition_in_bounds)]
+#![allow(clippy::unreadable_literal, clippy::type_repetition_in_bounds)]
 
 #[macro_use]
 extern crate failure;
