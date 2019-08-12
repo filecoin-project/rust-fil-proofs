db806454af04048e2cb2251e991ac658f95210c8
laser <l@s3r.me>
2019-08-12 16:40:10 -0700

    feat(write loads of debug output to stdout when benchy or micro fails)

--- fil-proofs-tooling/scripts/benchy.sh
+++ fil-proofs-tooling/scripts/benchy.sh
@@ -2,11 +2,12 @@
 
 which jq >/dev/null || { printf '%s\n' "error: jq" >&2; exit 1; }
 
-BENCHY_OUT=$(mktemp)
-TIME_OUT=$(mktemp)
+BENCHY_STDOUT=$(mktemp)
+GTIME_STDERR=$(mktemp)
+JQ_STDERR=$(mktemp)
 
-BIN="env time"
-CMD="-f '{ \"outputs\": { \"max-resident-set-size-kb\": %M } }' cargo run --quiet --bin benchy --release -- ${@} > ${BENCHY_OUT} 2> ${TIME_OUT}"
+GTIME_BIN="env time"
+CMD="-f '{ \"outputs\": { \"max-resident-set-size-kb\": %M } }' cargo run --quiet --bin benchy --release -- ${@} > ${BENCHY_STDOUT} 2> ${GTIME_STDERR}"
 
 if [[ $(env time --version 2>&1) != *"GNU"* ]]; then
     if [[ $(/usr/bin/time --version 2>&1) != *"GNU"* ]]; then
@@ -14,14 +15,35 @@ if [[ $(env time --version 2>&1) != *"GNU"* ]]; then
             printf '%s\n' "error: GNU time not installed" >&2
             exit 1
         else
-            BIN="gtime"
+            GTIME_BIN="gtime"
         fi
     else
-        BIN="/usr/bin/time"
+        GTIME_BIN="/usr/bin/time"
     fi
 fi
 
+eval "RUSTFLAGS=\"-Awarnings -C target-cpu=native\" ${GTIME_BIN} ${CMD}"
 
-eval "RUSTFLAGS=\"-Awarnings -C target-cpu=native\" ${BIN} ${CMD}"
+jq -s '.[0] * .[1]' $BENCHY_STDOUT $GTIME_STDERR 2> $JQ_STDERR
 
-jq -s '.[0] * .[1]' "${BENCHY_OUT}" "${TIME_OUT}"
+if [[ ! $? -eq 0 ]]; then
+    >&2 echo "*********************************************"
+    >&2 echo "* benchy failed - dumping debug information *"
+    >&2 echo "*********************************************"
+    >&2 echo ""
+    >&2 echo "<COMMAND>"
+    >&2 echo "${GTIME_BIN} ${CMD}"
+    >&2 echo "</COMMAND>"
+    >&2 echo ""
+    >&2 echo "<GTIME_STDERR>"
+    >&2 echo "$(cat $GTIME_STDERR)"
+    >&2 echo "</GTIME_STDERR>"
+    >&2 echo ""
+    >&2 echo "<BENCHY_STDOUT>"
+    >&2 echo "$(cat $BENCHY_STDOUT)"
+    >&2 echo "</BENCHY_STDOUT>"
+    >&2 echo ""
+    >&2 echo "<JQ_STDERR>"
+    >&2 echo "$(cat $JQ_STDERR)"
+    >&2 echo "</JQ_STDERR>"
+fi
--- fil-proofs-tooling/scripts/micro.sh
+++ fil-proofs-tooling/scripts/micro.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/env bash
 
-RUSTFLAGS="-Awarnings -C target-cpu=native" cargo run --quiet --bin micro --release ${@}
+MICRO_SDERR=$(mktemp)
+MICRO_SDOUT=$(mktemp)
+JQ_STDERR=$(mktemp)
+
+CMD="cargo run --quiet --bin micro --release ${@}"
+
+eval "RUSTFLAGS=\"-Awarnings -C target-cpu=native\" ${CMD}" 1> $MICRO_SDOUT 2> $MICRO_SDERR
+
+cat $MICRO_SDOUT | jq '.' 2> $JQ_STDERR
+
+if [[ ! $? -eq 0 ]]; then
+    >&2 echo "********************************************"
+    >&2 echo "* micro failed - dumping debug information *"
+    >&2 echo "********************************************"
+    >&2 echo ""
+    >&2 echo "<COMMAND>"
+    >&2 echo "${CMD}"
+    >&2 echo "</COMMAND>"
+    >&2 echo ""
+    >&2 echo "<MICRO_SDERR>"
+    >&2 echo "$(cat $MICRO_SDERR)"
+    >&2 echo "</MICRO_SDERR>"
+    >&2 echo ""
+    >&2 echo "<MICRO_SDOUT>"
+    >&2 echo "$(cat $MICRO_SDOUT)"
+    >&2 echo "</MICRO_SDOUT>"
+    >&2 echo ""
+    >&2 echo "<JQ_STDERR>"
+    >&2 echo "$(cat $JQ_STDERR)"
+    >&2 echo "</JQ_STDERR>"
+fi
