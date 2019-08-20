147dc4a2f6318807115c1a486207ca60b74596f0
laser <l@s3r.me>
2019-08-20 09:06:53 -0700

    fixup(point at branch)

--- fil-proofs-tooling/scripts/micro-remote.sh
+++ fil-proofs-tooling/scripts/micro-remote.sh
@@ -6,7 +6,7 @@ CMDS=$(cat <<EOF
 cd \$(mktemp -d)
 git clone -q https://github.com/filecoin-project/rust-fil-proofs.git
 cd rust-fil-proofs
-git checkout -q master
+git checkout -q feat/821-and-814-benchmark-improvements
 ./fil-proofs-tooling/scripts/retry.sh 42 10 60000 \
     ./fil-proofs-tooling/scripts/with-lock.sh 42 /tmp/benchmark \
     ./fil-proofs-tooling/scripts/micro.sh ${@:2}
