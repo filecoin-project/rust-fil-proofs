1a56a5a6e9d5fbb446ff83ccc8ae7eb42adb7d83
laser <l@s3r.me>
2019-08-20 13:33:39 -0700

    feat(persist JSON docs as CircleCI artifacts)

--- fil-proofs-tooling/scripts/micro-remote.sh
+++ fil-proofs-tooling/scripts/micro-remote.sh
@@ -6,7 +6,7 @@ CMDS=$(cat <<EOF
 cd \$(mktemp -d)
 git clone https://github.com/filecoin-project/rust-fil-proofs.git
 cd rust-fil-proofs
-git checkout -q feat/821-and-814-benchmark-improvements
+git checkout -q master
 ./fil-proofs-tooling/scripts/retry.sh 42 10 60000 \
     ./fil-proofs-tooling/scripts/with-lock.sh 42 /tmp/benchmark \
     ./fil-proofs-tooling/scripts/with-dots.sh \
