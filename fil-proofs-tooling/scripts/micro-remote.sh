2d1339bc74d5c0e4b1a9e7c97a01247790e8cd20
laser <l@s3r.me>
2019-08-20 13:18:50 -0700

    feat(add status-dots)

--- fil-proofs-tooling/scripts/micro-remote.sh
+++ fil-proofs-tooling/scripts/micro-remote.sh
@@ -9,6 +9,7 @@ cd rust-fil-proofs
 git checkout -q feat/821-and-814-benchmark-improvements
 ./fil-proofs-tooling/scripts/retry.sh 42 10 60000 \
     ./fil-proofs-tooling/scripts/with-lock.sh 42 /tmp/benchmark \
+    ./fil-proofs-tooling/scripts/with-dots.sh \
     ./fil-proofs-tooling/scripts/micro.sh ${@:2}
 EOF
 )
