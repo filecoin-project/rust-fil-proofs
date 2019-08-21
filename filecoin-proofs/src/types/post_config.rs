6943f660274bba8266b3772a2c2c28c0c352641b
dignifiedquire <dignifiedquire@users.noreply.github.com>
2019-08-21 14:07:07 +0200

    chore: happy clippy

--- filecoin-proofs/src/types/post_config.rs
+++ filecoin-proofs/src/types/post_config.rs
@@ -28,8 +28,8 @@ impl From<PoStConfig> for UnpaddedBytesAmount {
 
 impl PoStConfig {
     /// Returns the cache identifier as used by `storage-proofs::paramater_cache`.
-    pub fn get_cache_identifier(&self) -> String {
-        let params = crate::parameters::post_public_params(*self);
+    pub fn get_cache_identifier(self) -> String {
+        let params = crate::parameters::post_public_params(self);
 
         <RationalPoStCompound<DefaultTreeHasher> as CacheableParameters<
             Bls12,
@@ -38,17 +38,17 @@ impl PoStConfig {
         >>::cache_identifier(&params)
     }
 
-    pub fn get_cache_metadata_path(&self) -> PathBuf {
+    pub fn get_cache_metadata_path(self) -> PathBuf {
         let id = self.get_cache_identifier();
         parameter_cache::parameter_cache_metadata_path(&id)
     }
 
-    pub fn get_cache_verifying_key_path(&self) -> PathBuf {
+    pub fn get_cache_verifying_key_path(self) -> PathBuf {
         let id = self.get_cache_identifier();
         parameter_cache::parameter_cache_verifying_key_path(&id)
     }
 
-    pub fn get_cache_params_path(&self) -> PathBuf {
+    pub fn get_cache_params_path(self) -> PathBuf {
         let id = self.get_cache_identifier();
         parameter_cache::parameter_cache_params_path(&id)
     }
