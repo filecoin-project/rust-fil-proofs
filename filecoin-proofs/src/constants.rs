5c13f26232d356634d3bff07d56664d27d463b84
Lucas Molas <schomatis@gmail.com>
2019-09-17 20:41:28 -0300

    [REVERT] lower TEST_SECTOR_SIZE to 128

--- filecoin-proofs/src/constants.rs
+++ filecoin-proofs/src/constants.rs
@@ -4,7 +4,7 @@ pub const POREP_MINIMUM_CHALLENGES: usize = 12; // FIXME: 8,000
 pub const SINGLE_PARTITION_PROOF_LEN: usize = 192;
 
 // Sector size, in bytes, for tests.
-pub const TEST_SECTOR_SIZE: u64 = 1024;
+pub const TEST_SECTOR_SIZE: u64 = 128;
 
 // Sector size, in bytes, during live operation.
 pub const LIVE_SECTOR_SIZE: u64 = 1 << 28; // 256MiB
