944b1b561694285fb181e1c995a8aaf202a34bb9
laser <l@s3r.me>
2019-11-20 08:43:56 -0800

    ci(params): ensure benchmarking server has params

--- fil-proofs-tooling/scripts/paramcache-remote.sh
+++ fil-proofs-tooling/scripts/paramcache-remote.sh
@@ -7,10 +7,11 @@ cd \$(mktemp -d)
 git clone https://github.com/filecoin-project/rust-fil-proofs.git
 cd rust-fil-proofs
 git checkout -q $1
+export RUST_LOG=info
 ./fil-proofs-tooling/scripts/retry.sh 42 10 60000 \
     ./fil-proofs-tooling/scripts/with-lock.sh 42 /tmp/benchmark \
     ./fil-proofs-tooling/scripts/with-dots.sh \
-    RUST_LOG=info cargo run --release --package filecoin-proofs --bin=paramcache -- ${@:3}
+    cargo run --release --package filecoin-proofs --bin=paramcache -- ${@:3}
 EOF
 )
 
