1196a7e0b10e99a4d069ba5ea16a07871b144bc7
Friedel Ziegelmayer <me@dignifiedquire.com>
2019-12-11 17:36:13 +0100

    feat: more microbenchmarks (#991)

--- storage-proofs/benches/drgraph.rs
+++ storage-proofs/benches/drgraph.rs
@@ -3,23 +3,19 @@ use storage_proofs::drgraph::*;
 use storage_proofs::hasher::pedersen::*;
 
 fn drgraph(c: &mut Criterion) {
-    let params: Vec<_> = vec![12, 24, 128, 1024]
-        .iter()
-        .map(|n| {
-            (
-                BucketGraph::<PedersenHasher>::new(*n, BASE_DEGREE, 0, new_seed()).unwrap(),
-                2,
-            )
-        })
-        .collect();
+    let params = vec![12, 24, 128, 1024];
+
     c.bench(
         "sample",
         ParameterizedBenchmark::new(
             "bucket/m=6",
-            |b, (graph, i)| {
+            |b, n| {
+                let graph =
+                    BucketGraph::<PedersenHasher>::new(*n, BASE_DEGREE, 0, new_seed()).unwrap();
+
                 b.iter(|| {
                     let mut parents = vec![0; 6];
-                    black_box(graph.parents(*i, &mut parents).unwrap());
+                    black_box(graph.parents(2, &mut parents).unwrap());
                 })
             },
             params,
