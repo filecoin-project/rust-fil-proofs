5ec3ce4fc7f9707b72e58408696351fee236346b
laser <l@s3r.me>
2019-12-13 06:02:36 -0800

    feat(locking): make sys lock handle leaks
    The previous locking script didn't clean up leaks. This new iteration places a pid file in the lock directory and, when first run, checks to ensure that any existing lock corresponds to a running process (if not, the script relinquishes the lock on behalf of the now-dead process which acquired it).

--- fil-proofs-tooling/scripts/with-lock.sh
+++ fil-proofs-tooling/scripts/with-lock.sh
@@ -6,16 +6,31 @@ failure_code=$1
 lockdir=$2
 shift 2
 
+# Check to make sure that the process which owns the lock, if one exists, is
+# still alive. If the process is not alive, release the lock.
+for lockdir_pid in $(find $lockdir -type f -exec basename {} \; 2> /dev/null)
+do
+    if ! ps -p ${lockdir_pid} > /dev/null
+    then
+        (>&2 echo "cleaning up leaked lock (pid=${lockdir_pid}, path=${lockdir})")
+        rm -rf "${lockdir}"
+    fi
+done
+
 if mkdir "$lockdir" > /dev/null 2>&1
 then
-    (>&2 echo "successfully acquired lock (${lockdir})")
+    (>&2 echo "successfully acquired lock (pid=$$, path=${lockdir})")
 
-    # Unlock (by removing dir) when the script finishes
+    # Create a file to track the process id that acquired the lock. This
+    # is used to prevent leaks if the lock isn't relinquished correctly.
+    touch "$lockdir/$$"
+
+    # Unlock (by removing dir and pid file) when the script finishes.
     trap '(>&2 echo "relinquishing lock (${lockdir})"); rm -rf "$lockdir"' EXIT
 
     # Execute command
     "$@"
 else
-    (>&2 echo "failed to acquire lock (${lockdir})")
+    (>&2 echo "failed to acquire lock (path=${lockdir})")
     exit $failure_code
 fi
