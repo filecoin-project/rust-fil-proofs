f008bc3f8db61a657c9f678bbf358b639219d327
laser <l@s3r.me>
2019-12-13 04:23:51 -0800

    feat(metrics): consolidate metrics scripts + improve cleanup
    - clean up any temporary directories created by metrics runs after 1 day
    - consolidate all duplicated metrics scripts into single "run-remote.sh" script

---
+++ fil-proofs-tooling/scripts/run-remote.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+CMDS=$(cat <<EOF
+
+export RUST_LOG=info
+set -e
+
+# Creates a temporary directory in which we build rust-fil-proofs and capture
+# performance metrics. The name of the directory (today's UTC seconds plus 24
+# hours) serves as a cleanup mechanism; before metrics are captured, any expired
+# directories are removed.
+
+_one_day_from_now=\$((\$(date +%s) + 86400))
+_metrics_dir=/tmp/metrics/\$_one_day_from_now
+
+find /tmp/metrics/ -maxdepth 1 -mindepth 1 -type d -printf "%f\n" | xargs -I {} bash -c 'if (({} < \$(date +%s))) ; then rm -rf /tmp/metrics/{} ; fi' 2> /dev/null
+
+git clone -b $1 --single-branch https://github.com/filecoin-project/rust-fil-proofs.git \$_metrics_dir || true
+
+cd \$_metrics_dir
+
+./fil-proofs-tooling/scripts/retry.sh 42 10 60000 \
+    ./fil-proofs-tooling/scripts/with-lock.sh 42 /tmp/metrics.lock \
+    ./fil-proofs-tooling/scripts/with-dots.sh \
+    ${@:3}
+EOF
+)
+
+ssh -q $2 "$CMDS"
