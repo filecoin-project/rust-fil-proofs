509902f1a895f95671458a635b2cedeec88244ac
dignifiedquire <me@dignifiedquire.com>
2019-12-18 22:29:36 +0100

    add basic contribution code

--- filecoin-proofs/src/bin/phase2.rs
+++ filecoin-proofs/src/bin/phase2.rs
@@ -1,6 +1,6 @@
 use phase21 as phase2;
 use std::fs::File;
-use std::io::BufWriter;
+use std::io::{BufReader, BufWriter};
 
 use filecoin_proofs::constants::{
     DefaultPieceHasher, DefaultTreeHasher, DEFAULT_POREP_PROOF_PARTITIONS, SECTOR_SIZE_ONE_KIB,
@@ -11,12 +11,14 @@ use storage_proofs::circuit::stacked::StackedCompound;
 use storage_proofs::compound_proof::{self, CompoundProof};
 use storage_proofs::stacked::StackedDrg;
 
-fn main() {
+fn initial_setup() {
     let params = File::create("params").unwrap();
     let mut params = BufWriter::with_capacity(1024 * 1024, params);
 
     // Generate params for PoRep
     {
+        println!("Creating params for PoRep");
+
         // TODO: allow for different sizes
         let porep_config = PoRepConfig {
             sector_size: SectorSize(SECTOR_SIZE_ONE_KIB),
@@ -52,3 +54,33 @@ fn main() {
 
     // TODO: Generate params for PoSt
 }
+
+fn contribute() {
+    let params = File::create("params").unwrap();
+    let mut params_reader = BufReader::with_capacity(1024 * 1024, params);
+
+    println!("reading params from disk");
+    let mut params = phase2::MPCParameters::read(&mut params_reader, true).unwrap();
+
+    let mut rng = rand::rngs::OsRng; // TODO: allow participant to input randomness instead
+
+    let contribution = params.contribute(&mut rng);
+    println!("contributed: {}", hex::encode(&contribution[..]));
+
+    // TODO: add verification check
+}
+
+fn main() {
+    println!("Phase2 begins");
+    // TODO: make nice cli
+
+    // setup only run once
+    initial_setup();
+
+    // contribute
+    contribute();
+
+    println!("Phase2 has ended");
+}
+
+// TODO: add method for verification
