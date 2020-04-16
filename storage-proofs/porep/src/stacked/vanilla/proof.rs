a05bf83f493ba9b46f61b83c5b1708babe61c73d
nemo <nemo@protocol.ai>
2020-04-16 08:44:52 -0400

    feat: implement the most basic use of column tree builder

--- storage-proofs/porep/src/stacked/vanilla/proof.rs
+++ storage-proofs/porep/src/stacked/vanilla/proof.rs
@@ -3,15 +3,21 @@ use std::marker::PhantomData;
 use std::path::PathBuf;
 
 use generic_array::typenum::{self, Unsigned};
+use generic_array::{sequence::GenericSequence, GenericArray};
+
+use ff::Field;
 use log::{info, trace};
 use merkletree::merkle::{get_merkle_tree_len, is_merkle_tree_size_valid};
 use merkletree::store::{DiskStore, StoreConfig};
+use neptune::column_tree_builder::ColumnTreeBuilder;
+use paired::bls12_381::{Bls12, Fr};
 use rayon::prelude::*;
 use storage_proofs_core::{
     cache_key::CacheKey,
     data::Data,
     drgraph::Graph,
     error::Result,
+    fr32::fr_into_bytes,
     hasher::{Domain, HashFunction, Hasher},
     measurements::{
         measure_op,
@@ -26,13 +32,15 @@ use super::{
     column::Column,
     create_label, create_label_exp,
     graph::StackedBucketGraph,
-    hash::hash_single_column,
     params::{
         get_node, Labels, LabelsCache, PersistentAux, Proof, PublicInputs, PublicParams,
         ReplicaColumnProof, Tau, TemporaryAux, TemporaryAuxCache, TransformedLayers, BINARY_ARITY,
     },
     EncodingProof, LabelingProof,
 };
+//use typenum::{U1, U11, U2, U8};
+use typenum::{U1, U11, U8};
+
 use crate::encode::{decode, encode};
 use crate::PoRep;
 
@@ -484,10 +492,38 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> StackedDrg<'a, Tr
         let tree_c = measure_op(GenerateTreeC, || {
             info!("Building column hashes");
 
-            let mut trees = Vec::with_capacity(tree_count);
+            // FIXME: Need a better way to not hardcode params,
+            // preferably without requiring ColumnArity everywhere in
+            // stacked :-(
+            let get_builder = |nodes_count, layers| -> (_, _) {
+                match layers {
+                    // NOTE: This works for the "test_seal_lifecycle" tests
+                    1 => {
+                        let builder = ColumnTreeBuilder::<Bls12, U1, U8>::new(nodes_count);
+                        (
+                            builder,
+                            GenericArray::<Fr, U1>::generate(|_i: usize| Fr::zero()),
+                        )
+                    }
+                    /*
+                    // NOTE: This works for most other tests
+                    11 => {
+                        let builder = ColumnTreeBuilder::<Bls12, U11, U8>::new(nodes_count);
+                        (builder, GenericArray::<Fr, U11>::generate(|_i: usize| Fr::zero()))
+                    },
+                     */
+                    _ => panic!("fixme"),
+                }
+            };
+
+            let (mut column_builder, default_generic_array) = get_builder(nodes_count, layers);
+
             for (i, config) in configs.iter().enumerate() {
-                let mut hashes: Vec<<Tree::Hasher as Hasher>::Domain> =
-                    vec![<Tree::Hasher as Hasher>::Domain::default(); nodes_count];
+                let mut hashes = Vec::with_capacity(nodes_count);
+                // FIXME: better way to set default GenericArray list?
+                for _ in 0..nodes_count {
+                    hashes.push(default_generic_array);
+                }
 
                 rayon::scope(|s| {
                     // spawn n = num_cpus * 2 threads
@@ -499,40 +535,50 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> StackedDrg<'a, Tr
                     // chunk into n chunks
                     let chunk_size = (nodes_count as f64 / num_chunks as f64).ceil() as usize;
 
-                    // calculate all n chunks in parallel
+                    // gather all n chunks in parallel
                     for (chunk, hashes_chunk) in hashes.chunks_mut(chunk_size).enumerate() {
                         let labels = &labels;
 
                         s.spawn(move |_| {
                             for (j, hash) in hashes_chunk.iter_mut().enumerate() {
-                                let data: Vec<_> = (1..=layers)
-                                    .map(|layer| {
-                                        let store = labels.labels_for_layer(layer);
-                                        let el: <Tree::Hasher as Hasher>::Domain = store
-                                            .read_at((i * nodes_count) + j + chunk * chunk_size)
-                                            .unwrap();
-                                        el.into()
-                                    })
-                                    .collect();
+                                for k in 1..=layers {
+                                    let store = labels.labels_for_layer(k);
+                                    let el: <Tree::Hasher as Hasher>::Domain = store
+                                        .read_at((i * nodes_count) + j + chunk * chunk_size)
+                                        .unwrap();
 
-                                *hash = hash_single_column(&data).into();
+                                    hash[k - 1] = el.into();
+                                }
                             }
                         });
                     }
                 });
 
-                info!("building base tree_c {}/{}", i + 1, tree_count);
-                trees.push(DiskTree::<
-                    Tree::Hasher,
-                    Tree::Arity,
-                    typenum::U0,
-                    typenum::U0,
-                >::from_par_iter_with_config(
-                    hashes.into_par_iter(), config.clone()
-                )?);
+                info!("persisting base tree_c {}/{}", i + 1, tree_count);
+                let tree_data = column_builder.add_final_columns(&hashes)?;
+                let tree_data_len = tree_data.len();
+
+                // FIXME: Add store method to take iter for persisting elements directly?
+                // FIXME: Or have neptune return this as [u8]?
+                let mut flat_tree_data = Vec::with_capacity(
+                    tree_data_len * std::mem::size_of::<<Tree::Hasher as Hasher>::Domain>(),
+                );
+                for el in &tree_data {
+                    let cur = fr_into_bytes(&el);
+                    flat_tree_data.extend(&cur);
+                }
+                drop(tree_data);
+
+                // Persist the data to the store based on the current config.
+                DiskStore::<<Tree::Hasher as Hasher>::Domain>::new_from_slice_with_config(
+                    tree_data_len,
+                    Tree::Arity::to_usize(),
+                    &flat_tree_data,
+                    config.clone(),
+                )?;
+                drop(flat_tree_data);
             }
 
-            assert_eq!(tree_count, trees.len());
             create_disk_tree::<
                 DiskTree<Tree::Hasher, Tree::Arity, Tree::SubTreeArity, Tree::TopTreeArity>,
             >(tree_c_config.size.unwrap(), &configs)
