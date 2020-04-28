6071abf6b0692bb527b781ca790ce678d65aa2fe
dignifiedquire <me@dignifiedquire.com>
2020-04-28 20:38:46 +0200

    feat(storage-proofs): NSE - batch-hasher

--- storage-proofs/porep/src/nse/vanilla/mod.rs
+++ storage-proofs/porep/src/nse/vanilla/mod.rs
@@ -1,3 +1,4 @@
+mod batch_hasher;
 mod butterfly_graph;
 mod config;
 mod expander_graph;
@@ -5,6 +6,7 @@ mod expander_graph;
 /// A single parent index.
 pub type Parent = u32;
 
+pub use self::batch_hasher::batch_hash;
 pub use self::butterfly_graph::*;
 pub use self::config::Config;
 pub use self::expander_graph::*;
