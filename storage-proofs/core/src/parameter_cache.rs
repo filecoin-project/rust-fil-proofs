bf6e41c48227b1f989c878263b89056acf3fb1d6
nemo <nemo@protocol.ai>
2020-04-30 15:10:54 -0400

    fix: update CI cache key; some layer counts changed fix: update param version (testing, not required that we need these to be v27)

--- storage-proofs/core/src/parameter_cache.rs
+++ storage-proofs/core/src/parameter_cache.rs
@@ -16,7 +16,7 @@ use std::io::{self, SeekFrom};
 use std::path::{Path, PathBuf};
 
 /// Bump this when circuits change to invalidate the cache.
-pub const VERSION: usize = 26;
+pub const VERSION: usize = 27;
 
 pub const PARAMETER_CACHE_ENV_VAR: &str = "FIL_PROOFS_PARAMETER_CACHE";
 pub const PARAMETER_CACHE_DIR: &str = "/var/tmp/filecoin-proof-parameters/";
