97ffd2647eb4f98bdff71c5e96d716f56749958c
dignifiedquire <me@dignifiedquire.com>
2020-05-04 17:52:38 +0200

    feat(storage-proofs): NSE - layer generation

--- storage-proofs/porep/src/nse/vanilla/mod.rs
+++ storage-proofs/porep/src/nse/vanilla/mod.rs
@@ -2,6 +2,7 @@ mod batch_hasher;
 mod butterfly_graph;
 mod config;
 mod expander_graph;
+mod labels;
 
 /// A single parent index.
 pub type Parent = u32;
@@ -10,3 +11,5 @@ pub use self::batch_hasher::batch_hash;
 pub use self::butterfly_graph::*;
 pub use self::config::Config;
 pub use self::expander_graph::*;
+
+pub(crate) use self::labels::*;
