22d5d04819743ba05905583d101f2a6a4bc260d5
dignifiedquire <me@dignifiedquire.com>
2020-05-06 17:20:38 +0200

    feat(storage-proofs): NSE  - test replication

--- storage-proofs/porep/src/nse/vanilla/porep.rs
+++ storage-proofs/porep/src/nse/vanilla/porep.rs
@@ -33,8 +33,11 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> PoRep<'a, Tree::H
         _replica_path: PathBuf,
     ) -> Result<(Self::Tau, Self::ProverAux)> {
         let config = &pp.config;
+        let num_nodes_sector = config.num_nodes_sector();
 
-        let num_nodes_sector = config.sector_size / NODE_SIZE;
+        assert_eq!(num_nodes_sector % config.num_nodes_window, 0);
+        assert_eq!(data.len(), config.sector_size);
+        assert_eq!(config.sector_size % config.window_size(), 0);
 
         // ensure there is a data tree
         let mut data_tree_config = StoreConfig::from_config(
@@ -59,8 +62,7 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> PoRep<'a, Tree::H
         // phase 1
         // replicate each window and build its tree
 
-        let mut layered_trees: Vec<Vec<_>> =
-            (0..config.num_windows()).map(|_| Vec::new()).collect();
+        let mut layered_trees: Vec<Vec<_>> = (0..config.num_layers()).map(|_| Vec::new()).collect();
 
         for (window_index, window_data) in
             data.as_mut().chunks_mut(config.window_size()).enumerate()
@@ -85,6 +87,7 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> PoRep<'a, Tree::H
         let mut layer_configs = Vec::new();
         for trees_configs in layered_trees.into_iter() {
             let (trees, configs): (Vec<_>, Vec<_>) = trees_configs.into_iter().unzip();
+
             trees_layers.push(LCTree::<
                 Tree::Hasher,
                 U8,
@@ -135,5 +138,92 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> PoRep<'a, Tree::H
         _config: Option<StoreConfig>,
     ) -> Result<Vec<u8>> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use generic_array::typenum::U0;
+    use rand::SeedableRng;
+    use rand_xorshift::XorShiftRng;
+    use storage_proofs_core::{
+        hasher::{PoseidonHasher, Sha256Hasher},
+        proof::ProofScheme,
+    };
+
+    use super::super::{Config, SetupParams};
+
+    #[test]
+    fn test_extract_all() {
+        type Tree = LCTree<PoseidonHasher, U8, U8, U0>;
+        // femme::pretty::Logger::new()
+        //     .start(log::LevelFilter::Trace)
+        //     .ok();
+
+        let rng = &mut XorShiftRng::from_seed(crate::TEST_SEED);
+        let replica_id = <PoseidonHasher as Hasher>::Domain::random(rng);
+        let config = Config {
+            k: 8,
+            num_nodes_window: 64,
+            degree_expander: 12,
+            degree_butterfly: 8,
+            num_expander_layers: 3,
+            num_butterfly_layers: 3,
+            sector_size: 64 * 32 * 8,
+        };
+
+        let data: Vec<u8> = (0..config.num_nodes_sector())
+            .flat_map(|_| {
+                let v = <PoseidonHasher as Hasher>::Domain::random(rng);
+                v.into_bytes()
+            })
+            .collect();
+
+        // create a copy, so we can compare roundtrips
+        let mut data_copy = data.clone();
+
+        let sp = SetupParams {
+            config: config.clone(),
+        };
+
+        let pp = NarrowStackedExpander::<Tree, Sha256Hasher>::setup(&sp).expect("setup failed");
+
+        // MT for original data is always named tree-d, and it will be
+        // referenced later in the process as such.
+        let cache_dir = tempfile::tempdir().unwrap();
+        let config = StoreConfig::new(
+            cache_dir.path(),
+            CacheKey::CommDTree.to_string(),
+            StoreConfig::default_cached_above_base_layer(config.num_nodes_sector(), U2::to_usize()),
+        );
+
+        // Generate a replica path.
+        let temp_dir = tempdir::TempDir::new("test-extract-all").unwrap();
+        let temp_path = temp_dir.path();
+        let replica_path = temp_path.join("replica-path");
+
+        NarrowStackedExpander::<Tree, PoseidonHasher>::replicate(
+            &pp,
+            &replica_id,
+            (&mut data_copy[..]).into(),
+            None,
+            config.clone(),
+            replica_path.clone(),
+        )
+        .expect("replication failed");
+
+        assert_ne!(data, data_copy);
+
+        let decoded_data = NarrowStackedExpander::<Tree, PoseidonHasher>::extract_all(
+            &pp,
+            &replica_id,
+            data_copy.as_mut_slice(),
+            Some(config.clone()),
+        )
+        .expect("failed to extract data");
+
+        assert_eq!(data, decoded_data);
     }
 }
