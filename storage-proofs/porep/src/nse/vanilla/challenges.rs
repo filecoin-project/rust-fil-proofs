508255af2d0001011dbf898fc8deaea6e8dc00c4
dignifiedquire <me@dignifiedquire.com>
2020-05-06 10:41:35 +0200

    feat(storage-proofs): NSE - structure for proof and proof-scheme

---
+++ storage-proofs/porep/src/nse/vanilla/challenges.rs
@@ -0,0 +1,4 @@
+#[derive(Debug, Default)]
+pub struct ChallengeRequirements {
+    pub minimum_challenges: usize,
+}
--- storage-proofs/porep/src/nse/vanilla/mod.rs
+++ storage-proofs/porep/src/nse/vanilla/mod.rs
@@ -1,15 +1,21 @@
 mod batch_hasher;
 mod butterfly_graph;
+mod challenges;
 mod config;
 mod expander_graph;
 mod labels;
+mod nse;
+mod porep;
+mod proof_scheme;
+mod replicate;
 
 /// A single parent index.
 pub type Parent = u32;
 
 pub use self::batch_hasher::batch_hash;
 pub use self::butterfly_graph::*;
+pub use self::challenges::*;
 pub use self::config::Config;
 pub use self::expander_graph::*;
-
-pub(crate) use self::labels::*;
+pub use self::labels::*;
+pub use self::nse::*;
