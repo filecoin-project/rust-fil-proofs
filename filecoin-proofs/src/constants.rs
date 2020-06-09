69a6fbd5c12f80e790c40fe62512a798d1320c2b
porcuquine <porcuquine@users.noreply.github.com>
2020-06-08 22:37:13 -0700

    Updated reported PoSt constraints (in comments).

--- filecoin-proofs/src/constants.rs
+++ filecoin-proofs/src/constants.rs
@@ -95,8 +95,8 @@ lazy_static! {
             (SECTOR_SIZE_16_MIB, 2),
             (SECTOR_SIZE_512_MIB, 2),
             (SECTOR_SIZE_1_GIB, 2),
-            (SECTOR_SIZE_32_GIB, 2349), // this gives 133,977,564 constraints, fitting just in a single partition
-            (SECTOR_SIZE_64_GIB, 2300), // this gives 131,182,800 constraints, fitting just in a single partition
+            (SECTOR_SIZE_32_GIB, 2349), // this gives 125,279,217 constraints, fitting in a single partition
+            (SECTOR_SIZE_64_GIB, 2300), // this gives 129,887,900 constraints, fitting in a single partition
         ]
         .iter()
         .copied()
