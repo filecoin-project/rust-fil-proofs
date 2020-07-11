9ccdd16bab8ed893008c679d87c65fdde0e59809
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-07-11 13:45:48 +0430

    refactoring

--- fil-proofs-tooling/src/metadata.rs
+++ fil-proofs-tooling/src/metadata.rs
@@ -66,7 +66,6 @@ pub struct SystemMetadata {
 }
 
 impl SystemMetadata {
-    #[cfg(target_arch = "x86_64")]
     pub fn new() -> Result<Self> {
         use async_std::task::block_on;
         let host = block_on(async { heim::host::platform().await })
@@ -78,20 +77,38 @@ impl SystemMetadata {
         let cpu_physical = block_on(async { heim::cpu::physical_count().await })
             .map_err(|_| anyhow!("Failed to retrieve cpu physical count information"))?;
 
-        let cpuid = raw_cpuid::CpuId::new();
-        let processor = cpuid
-            .get_extended_function_info()
-            .and_then(|info| info.processor_brand_string().map(|s| s.to_string()))
-            .unwrap_or_default();
-        let (base, max) = cpuid
-            .get_processor_frequency_info()
-            .map(|info| {
+        let (processor, base, max, features) = {
+            #[cfg(target_arch = "x86_64")]
+            {
+                let cpuid = raw_cpuid::CpuId::new();
+                let processor = cpuid
+                    .get_extended_function_info()
+                    .and_then(|info| info.processor_brand_string().map(|s| s.to_string()))
+                    .unwrap_or_default();
+                let (base, max) = cpuid
+                    .get_processor_frequency_info()
+                    .map(|info| {
+                        (
+                            info.processor_base_frequency(),
+                            info.processor_max_frequency(),
+                        )
+                    })
+                    .unwrap_or_default();
                 (
-                    info.processor_base_frequency(),
-                    info.processor_max_frequency(),
+                    processor,
+                    base,
+                    max,
+                    cpuid
+                        .get_feature_info()
+                        .map(|info| format!("{:?}", info))
+                        .unwrap_or_default(),
                 )
-            })
-            .unwrap_or_default();
+            }
+            #[cfg(not(target_arch = "x86_64"))]
+            {
+                ("unknown".into(), 0, 0, "unknown".into())
+            }
+        };
 
         Ok(SystemMetadata {
             system: host.system().into(),
@@ -101,37 +118,7 @@ impl SystemMetadata {
             processor,
             processor_base_frequency_hz: base,
             processor_max_frequency_hz: max,
-            processor_features: cpuid
-                .get_feature_info()
-                .map(|info| format!("{:?}", info))
-                .unwrap_or_default(),
-            processor_cores_logical: cpu_logical,
-            processor_cores_physical: cpu_physical.unwrap_or_default(),
-            memory_total_bytes: memory.total().get::<uom::si::information::byte>(),
-        })
-    }
-
-    #[cfg(not(target_arch = "x86_64"))]
-    pub fn new() -> Result<Self> {
-        use async_std::task::block_on;
-        let host = block_on(async { heim::host::platform().await })
-            .map_err(|_| anyhow!("Failed to retrieve host information"))?;
-        let memory = block_on(async { heim::memory::memory().await })
-            .map_err(|_| anyhow!("Failed to retrieve memory information"))?;
-        let cpu_logical = block_on(async { heim::cpu::logical_count().await })
-            .map_err(|_| anyhow!("Failed to retrieve cpu logical count information"))?;
-        let cpu_physical = block_on(async { heim::cpu::physical_count().await })
-            .map_err(|_| anyhow!("Failed to retrieve cpu physical count information"))?;
-
-        Ok(SystemMetadata {
-            system: host.system().into(),
-            release: host.release().into(),
-            version: host.version().into(),
-            architecture: host.architecture().as_str().into(),
-            processor: "unknown".into(),
-            processor_base_frequency_hz: 0,
-            processor_max_frequency_hz: 0,
-            processor_features: "unknown".into(),
+            processor_features: features,
             processor_cores_logical: cpu_logical,
             processor_cores_physical: cpu_physical.unwrap_or_default(),
             memory_total_bytes: memory.total().get::<uom::si::information::byte>(),
