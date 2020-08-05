95b17706b0c121c8e6156c8d194915a7f45a934d
nemo <nemo@protocol.ai>
2020-08-05 12:55:18 -0400

    feat: add a check parameter command that maps parameter files (#1238)

---
+++ fil-proofs-tooling/src/bin/check_parameters/main.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use bellperson::groth16::MappedParameters;
+use clap::{value_t, App, Arg, SubCommand};
+use paired::bls12_381::Bls12;
+
+use storage_proofs::parameter_cache::read_cached_params;
+
+fn run_map(parameter_file: &PathBuf) -> Result<MappedParameters<Bls12>> {
+    read_cached_params(parameter_file)
+}
+
+fn main() -> Result<()> {
+    fil_logger::init();
+
+    let map_cmd = SubCommand::with_name("map")
+        .about("build mapped parameters")
+        .arg(
+            Arg::with_name("param")
+                .long("parameter-file")
+                .help("The parameter file to map")
+                .required(true)
+                .takes_value(true),
+        );
+
+    let matches = App::new("check_parameters")
+        .version("0.1")
+        .subcommand(map_cmd)
+        .get_matches();
+
+    match matches.subcommand() {
+        ("map", Some(m)) => {
+            let parameter_file = value_t!(m, "param", PathBuf)?;
+            run_map(&parameter_file)?;
+        }
+        _ => panic!("Unrecognized subcommand"),
+    }
+
+    Ok(())
+}
