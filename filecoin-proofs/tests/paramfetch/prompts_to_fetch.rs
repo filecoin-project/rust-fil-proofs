adc25bd8968df0e13299514b6106b6d0ad0eca30
nemo <nemo@protocol.ai>
2020-09-14 07:50:48 -0400

    fix: update parameters.json location

--- filecoin-proofs/tests/paramfetch/prompts_to_fetch.rs
+++ filecoin-proofs/tests/paramfetch/prompts_to_fetch.rs
@@ -186,7 +186,7 @@ fn invalid_json_produces_error() -> Result<(), FailureError> {
 
 #[test]
 fn no_json_path_uses_default_manifest() -> Result<(), FailureError> {
-    let file = File::open("parameters.json")?;
+    let file = File::open("../storage-proofs/parameters.json")?;
     let reader = BufReader::new(file);
     let manifest: ParameterMap = serde_json::from_reader(reader)?;
 
