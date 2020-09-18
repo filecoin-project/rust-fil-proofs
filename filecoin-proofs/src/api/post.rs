9117ed68fbf73cc32f34c1440120393cbab639f1
nemo <nemo@protocol.ai>
2020-09-18 17:55:55 -0400

    fix: remove artificial requirement that config sector count be 1 (#1283)
    This is in the context of generating a single vanilla proof.  A specialized post config should not be required.

--- filecoin-proofs/src/api/post.rs
+++ filecoin-proofs/src/api/post.rs
@@ -537,17 +537,7 @@ pub fn generate_single_vanilla_proof<Tree: 'static + MerkleTreeTrait>(
     let comm_c = replica.safe_comm_c();
     let comm_r_last = replica.safe_comm_r_last();
 
-    // There is only enough information in the arguments to generate a
-    // single vanilla proof, so the behaviour is unexpected if the
-    // sector_count provided is > 1.
-    let sector_count = post_config.sector_count;
-    ensure!(
-        post_config.sector_count == 1,
-        "invalid post config sector size ({} required)",
-        sector_count
-    );
-
-    let mut priv_sectors = Vec::with_capacity(sector_count);
+    let mut priv_sectors = Vec::with_capacity(1);
     priv_sectors.push(fallback::PrivateSector {
         tree,
         comm_c,
