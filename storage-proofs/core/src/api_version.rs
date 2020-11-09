27a2aa42b42380b2dc47379ed618fe1241e7a6f9
nemo <nemo@protocol.ai>
2020-11-09 13:26:13 -0500

    style: rust fmt

--- storage-proofs/core/src/api_version.rs
+++ storage-proofs/core/src/api_version.rs
@@ -14,14 +14,12 @@ impl FromStr for ApiVersion {
     fn from_str(api_version_str: &str) -> Result<Self> {
         let api_version = Version::parse(api_version_str)?;
         match api_version.major {
-            1 => {
-                match api_version.minor {
-                    0 => Ok(ApiVersion::V1_0), 
-                    1 => Ok(ApiVersion::V1_1),
-                    _ => Err(anyhow::format_err!(
-                        "Could not parse API Version from string"
-                    )),
-                }
+            1 => match api_version.minor {
+                0 => Ok(ApiVersion::V1_0),
+                1 => Ok(ApiVersion::V1_1),
+                _ => Err(anyhow::format_err!(
+                    "Could not parse API Version from string"
+                )),
             },
             _ => Err(anyhow::format_err!(
                 "Could not parse API Version from string"
