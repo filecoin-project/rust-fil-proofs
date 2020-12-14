d4aed8b53b2a09d67824d1169e3c4f2d0cc7aad3
nemo <nemo@protocol.ai>
2020-12-14 14:53:05 -0500

    fix: fix merkle bench for poseidon hashing

--- storage-proofs/core/benches/merkle.rs
+++ storage-proofs/core/benches/merkle.rs
@@ -1,9 +1,12 @@
+use anyhow::Result;
 use criterion::{black_box, criterion_group, criterion_main, Criterion, ParameterizedBenchmark};
-use filecoin_hashers::{poseidon::PoseidonHasher, sha256::Sha256Hasher};
+use filecoin_hashers::{
+    poseidon::PoseidonDomain, poseidon::PoseidonHasher, sha256::Sha256Hasher, Domain,
+};
 use rand::{thread_rng, Rng};
 use storage_proofs_core::merkle::{create_base_merkle_tree, BinaryMerkleTree};
 
-fn merkle_benchmark(c: &mut Criterion) {
+fn merkle_benchmark_sha256(c: &mut Criterion) {
     #[cfg(feature = "big-sector-sizes-bench")]
     let params = vec![128, 1024, 1_048_576];
     #[cfg(not(feature = "big-sector-sizes-bench"))]
@@ -26,23 +29,44 @@ fn merkle_benchmark(c: &mut Criterion) {
                 })
             },
             params,
-        )
-        .with_function("poseidon", move |b, n_nodes| {
-            let mut rng = thread_rng();
-            let data: Vec<u8> = (0..32 * *n_nodes).map(|_| rng.gen()).collect();
-
-            b.iter(|| {
-                black_box(
-                    create_base_merkle_tree::<BinaryMerkleTree<PoseidonHasher>>(
-                        None, *n_nodes, &data,
-                    )
-                    .unwrap(),
-                )
-            })
-        })
-        .sample_size(20),
+        ),
     );
 }
 
-criterion_group!(benches, merkle_benchmark);
+fn merkle_benchmark_poseidon(c: &mut Criterion) {
+    #[cfg(feature = "big-sector-sizes-bench")]
+    let params = vec![64];
+    #[cfg(not(feature = "big-sector-sizes-bench"))]
+    let params = vec![64];
+
+    c.bench(
+        "merkletree-binary",
+        ParameterizedBenchmark::new(
+            "poseidon",
+            move |b, n_nodes| {
+                let mut rng = thread_rng();
+                let mut data: Vec<u8> = Vec::with_capacity(32 * *n_nodes);
+                (0..*n_nodes)
+                    .into_iter()
+                    .try_for_each(|_| -> Result<()> {
+                        let node = PoseidonDomain::random(&mut rng);
+                        Ok(data.extend(node.into_bytes()))
+                    })
+                    .expect("failed to generate data");
+
+                b.iter(|| {
+                    black_box(
+                        create_base_merkle_tree::<BinaryMerkleTree<PoseidonHasher>>(
+                            None, *n_nodes, &data,
+                        )
+                        .unwrap(),
+                    )
+                })
+            },
+            params,
+        ),
+    );
+}
+
+criterion_group!(benches, merkle_benchmark_sha256, merkle_benchmark_poseidon);
 criterion_main!(benches);
