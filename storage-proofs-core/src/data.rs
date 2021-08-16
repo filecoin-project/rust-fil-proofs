3cc84dce3a813dc82c0196e7ade85c9b31828f6a
nemo <nemo@protocol.ai>
2021-08-16 11:30:19 -0400

    fix: return result on drop_data and fix clippy

--- storage-proofs-core/src/data.rs
+++ storage-proofs-core/src/data.rs
@@ -138,18 +138,17 @@ impl<'a> Data<'a> {
     }
 
     /// Drops the actual data, if we can recover it.
-    pub fn drop_data(&mut self) {
+    pub fn drop_data(&mut self) -> Result<()> {
         if let Some(ref p) = self.path {
             info!("dropping data {}", p.display());
 
-            match &self.raw {
-                Some(RawData::Mmap(raw)) => {
-                    raw.flush().expect("mmap flush failure");
-                }
-                _ => {}
-            };
+            if let Some(RawData::Mmap(raw)) = &self.raw {
+                raw.flush()?;
+            }
 
             self.raw.take();
         }
+
+        Ok(())
     }
 }
