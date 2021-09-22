aa4353f47a9ba28ad44c8837679dfb36b953ff97
nemo <nemo@protocol.ai>
2021-09-22 11:09:29 -0400

    feat: add a generate update proof stub API call

--- filecoin-proofs/src/api/update.rs
+++ filecoin-proofs/src/api/update.rs
@@ -92,10 +92,11 @@ pub fn encode_into<Tree: 'static + MerkleTreeTrait>(
     staged_data_path: &Path,
     piece_infos: &[PieceInfo],
     comm_sector_key: Commitment,
-) -> Result<(Commitment, Commitment)> {
+) -> Result<(Commitment, Commitment, Commitment)> {
     info!("encode_into:start");
-    let mut comm_r = [0; 32];
     let mut comm_d = [0; 32];
+    let mut comm_r = [0; 32];
+    let mut comm_r_last = [0; 32];
 
     // NOTE: p_aux has comm_c and comm_r_last
     let p_aux: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
@@ -125,29 +126,36 @@ pub fn encode_into<Tree: 'static + MerkleTreeTrait>(
             .context("failed to restore contents of t_aux")?;
 
     let nodes_count = u64::from(porep_config.sector_size) as usize / NODE_SIZE;
-    let (comm_r_domain, comm_d_domain) = CCUpdateVanilla::<Tree, DefaultPieceHasher>::encode_into(
-        nodes_count,
-        &t_aux_cache,
-        <Tree::Hasher as Hasher>::Domain::try_from_bytes(&p_aux.comm_c.into_bytes())?,
-        <Tree::Hasher as Hasher>::Domain::try_from_bytes(&comm_sector_key)?,
-        &new_replica_path,
-        &new_cache_path,
-        &sector_key_path,
-        &sector_key_cache_path,
-        &staged_data_path,
-    )?;
+    let (comm_r_domain, comm_r_last_domain, comm_d_domain) =
+        CCUpdateVanilla::<Tree, DefaultPieceHasher>::encode_into(
+            nodes_count,
+            &t_aux_cache,
+            <Tree::Hasher as Hasher>::Domain::try_from_bytes(&p_aux.comm_c.into_bytes())?,
+            <Tree::Hasher as Hasher>::Domain::try_from_bytes(&comm_sector_key)?,
+            &new_replica_path,
+            &new_cache_path,
+            &sector_key_path,
+            &sector_key_cache_path,
+            &staged_data_path,
+        )?;
 
-    comm_r_domain.write_bytes(&mut comm_r)?;
     comm_d_domain.write_bytes(&mut comm_d)?;
+    comm_r_domain.write_bytes(&mut comm_r)?;
+    comm_r_last_domain.write_bytes(&mut comm_r_last)?;
+
     ensure!(comm_d != [0; 32], "Invalid all zero commitment (comm_d)");
     ensure!(comm_r != [0; 32], "Invalid all zero commitment (comm_r)");
+    ensure!(
+        comm_r_last != [0; 32],
+        "Invalid all zero commitment (comm_r)"
+    );
     ensure!(
         verify_pieces(&comm_d, piece_infos, porep_config.into())?,
         "pieces and comm_d do not match"
     );
 
     info!("encode_into:finish");
-    Ok((comm_r, comm_d))
+    Ok((comm_r, comm_r_last, comm_d))
 }
 
 /// Reverses the encoding process and outputs the data into out_data_path.
@@ -216,22 +224,38 @@ pub fn generate_update_proof<Tree: 'static + MerkleTreeTrait>(
     porep_config: PoRepConfig,
     comm_r_old: Commitment,
     comm_r_new: Commitment,
+    comm_r_last_new: Commitment,
     comm_d_new: Commitment,
+    sector_key_path: &Path,
+    sector_key_cache_path: &Path,
     replica_path: &Path,
     replica_cache_path: &Path,
 ) -> Result<()> {
     // FIXME: Return UpdateProof type
+    info!("generate_update_proof:start");
 
-    /*
+    let nodes_count = u64::from(porep_config.sector_size) as usize / NODE_SIZE;
+
+    // Note: t_aux has labels and tree_d, tree_c, tree_r_last store configs
+    let t_aux = {
+        let t_aux_path = sector_key_cache_path.join(CacheKey::TAux.to_string());
+        let t_aux_bytes = fs::read(&t_aux_path)
+            .with_context(|| format!("could not read file t_aux={:?}", t_aux_path))?;
+
+        let res: TemporaryAux<_, _> = deserialize(&t_aux_bytes)?;
+        res
+    };
+
     // Convert TemporaryAux to TemporaryAuxCache, which instantiates all
     // elements based on the configs stored in TemporaryAux.
     let t_aux_cache: TemporaryAuxCache<Tree, DefaultPieceHasher> =
-        TemporaryAuxCache::new(&t_aux, replica_path.as_ref().to_path_buf())
+        TemporaryAuxCache::new(&t_aux, sector_key_cache_path.to_path_buf())
             .context("failed to restore contents of t_aux")?;
-     */
 
     let comm_r_old_safe = <Tree::Hasher as Hasher>::Domain::try_from_bytes(&comm_r_old)?;
     let comm_r_new_safe = <Tree::Hasher as Hasher>::Domain::try_from_bytes(&comm_r_new)?;
+    let comm_r_last_new_safe = <Tree::Hasher as Hasher>::Domain::try_from_bytes(&comm_r_last_new)?;
+
     let comm_d_new_safe = DefaultPieceDomain::try_from_bytes(&comm_d_new)?;
 
     let public_params: storage_proofs_update::PublicParams =
@@ -245,11 +269,20 @@ pub fn generate_update_proof<Tree: 'static + MerkleTreeTrait>(
         h_select: u64::from(HSelect::from(porep_config)),
         _tree_r: PhantomData::default(),
     };
-    /*
-    let private_inputs = stacked::PrivateInputs::<Tree, DefaultPieceHasher> {
-        p_aux,
-        t_aux: t_aux_cache,
-    };*/
 
-    Ok(())
+    /*let update_proof = */
+    CCUpdateVanilla::<Tree, DefaultPieceHasher>::generate_update_proof(
+        nodes_count,
+        public_params,
+        public_inputs,
+        &t_aux_cache,
+        comm_r_old_safe,
+        comm_r_last_new_safe,
+        replica_path,
+        replica_cache_path,
+    )?;
+
+    info!("generate_update_proof:finish");
+
+    Ok(/*update_proof*/ ())
 }
