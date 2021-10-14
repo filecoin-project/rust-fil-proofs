9c4f840b84445bb8fb152579ad5f1690e90a96b6
nemo <nemo@protocol.ai>
2021-10-14 10:00:08 -0400

    feat: clean-up p_aux/t_aux usage

--- filecoin-proofs/src/api/update.rs
+++ filecoin-proofs/src/api/update.rs
@@ -31,6 +31,75 @@ use crate::{
     },
 };
 
+// Instantiates p_aux from the specified cache_dir for access to comm_c and comm_r_last
+fn get_p_aux<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHasher>>(
+    cache_path: &Path,
+) -> Result<PersistentAux<<Tree::Hasher as Hasher>::Domain>> {
+    let p_aux_path = cache_path.join(CacheKey::PAux.to_string());
+    let p_aux_bytes = fs::read(&p_aux_path)
+        .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
+
+    let p_aux = deserialize(&p_aux_bytes)?;
+
+    Ok(p_aux)
+}
+
+// Instantiates t_aux from the specified cache_dir for access to
+// labels and tree_d, tree_c, tree_r_last store configs
+fn get_t_aux<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHasher>>(
+    cache_path: &Path,
+) -> Result<TemporaryAux<Tree, DefaultPieceHasher>> {
+    let t_aux_path = cache_path.join(CacheKey::TAux.to_string());
+    let t_aux_bytes = fs::read(&t_aux_path)
+        .with_context(|| format!("could not read file t_aux={:?}", t_aux_path))?;
+
+    let res: TemporaryAux<Tree, DefaultPieceHasher> = deserialize(&t_aux_bytes)?;
+
+    Ok(res)
+}
+
+// Re-instantiate a t_aux with the new cache path, then use the tree_d
+// and tree_r_last configs from it.  This is done to preserve the
+// original tree configuration info (in particular, the
+// 'rows_to_discard' value) rather than re-setting it to the default
+// in case it was not created with the default.
+//
+// If we are sure that this doesn't matter, it would be much simpler
+// to just create new configs, e.g. StoreConfig::new(new_cache_path,
+// ...)
+//
+// Returns a pair of the new tree_d_config and tree_r_last configs
+fn get_new_configs_from_t_aux_old<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHasher>>(
+    t_aux_old: &TemporaryAux<Tree, DefaultPieceHasher>,
+    new_cache_path: &Path,
+    nodes_count: usize,
+) -> Result<(StoreConfig, StoreConfig)> {
+    let mut t_aux_new = t_aux_old.clone();
+    t_aux_new.set_cache_path(new_cache_path);
+
+    let tree_count = get_base_tree_count::<Tree>();
+    let base_tree_nodes_count = nodes_count / tree_count;
+
+    // With the new cache path set, formulate the new tree_d and
+    // tree_r_last configs.
+    let tree_d_new_config = StoreConfig::from_config(
+        &t_aux_new.tree_d_config,
+        CacheKey::CommDTree.to_string(),
+        Some(get_merkle_tree_len(nodes_count, TreeDArity::to_usize())?),
+    );
+
+    let tree_r_last_new_config = StoreConfig::from_config(
+        &t_aux_new.tree_r_last_config,
+        CacheKey::CommRLastTree.to_string(),
+        Some(get_merkle_tree_len(
+            base_tree_nodes_count,
+            Tree::Arity::to_usize(),
+        )?),
+    );
+
+    Ok((tree_d_new_config, tree_r_last_new_config))
+}
+
 /// Encodes data into an existing replica.  The original replica is
 /// not modified and the resulting output data is written as
 /// new_replica_path (with required artifacts located in
@@ -47,65 +116,16 @@ pub fn encode_into<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHasher>>(
 ) -> Result<EmptySectorUpdateEncoded> {
     info!("encode_into:start");
     let config = SectorUpdateConfig::from_porep_config(porep_config);
-    let nodes_count = config.nodes_count;
-    let tree_count = get_base_tree_count::<Tree>();
-    let base_tree_nodes_count = nodes_count / tree_count;
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
+    let p_aux = get_p_aux::<Tree>(sector_key_cache_path)?;
+    let t_aux = get_t_aux::<Tree>(sector_key_cache_path)?;
 
-        deserialize(&p_aux_bytes)
-    }?;
-
-    // Note: t_aux has labels and tree_d, tree_c, tree_r_last store configs
-    let t_aux = {
-        let t_aux_path = sector_key_cache_path.join(CacheKey::TAux.to_string());
-        let t_aux_bytes = fs::read(&t_aux_path)
-            .with_context(|| format!("could not read file t_aux={:?}", t_aux_path))?;
-
-        let mut res: TemporaryAux<Tree, DefaultPieceHasher> = deserialize(&t_aux_bytes)?;
-        // Switch t_aux to the passed in cache_path
-        res.set_cache_path(sector_key_cache_path);
-        res
-    };
-
-    // Re-instantiate a t_aux with the new cache path, then use the
-    // tree_d and tree_r_last configs from it.  This is done to
-    // preserve the original tree configuration info (in particular,
-    // the 'rows_to_discard' value) rather than re-setting it to the
-    // default in case it was not created with the default.
-    //
-    // If we are sure that this doesn't matter, it would be much
-    // simpler to just create new configs,
-    // e.g. StoreConfig::new(new_cache_path, ...)
-    let mut t_aux_new = t_aux;
-    t_aux_new.set_cache_path(new_cache_path);
-
-    // With the new cache path set, formulate the new tree_d and tree_r_last configs.
-    let tree_d_new_config = StoreConfig::from_config(
-        &t_aux_new.tree_d_config,
-        CacheKey::CommDTree.to_string(),
-        Some(get_merkle_tree_len(
-            base_tree_nodes_count,
-            TreeDArity::to_usize(),
-        )?),
-    );
-
-    let tree_r_last_new_config = StoreConfig::from_config(
-        &t_aux_new.tree_r_last_config,
-        CacheKey::CommRLastTree.to_string(),
-        Some(get_merkle_tree_len(
-            base_tree_nodes_count,
-            Tree::Arity::to_usize(),
-        )?),
-    );
+    let (tree_d_new_config, tree_r_last_new_config) =
+        get_new_configs_from_t_aux_old::<Tree>(&t_aux, new_cache_path, config.nodes_count)?;
 
     let (comm_r_domain, comm_r_last_domain, comm_d_domain) =
         EmptySectorUpdate::<Tree>::encode_into(
-            nodes_count,
+            config.nodes_count,
             tree_d_new_config,
             tree_r_last_new_config,
             <Tree::Hasher as Hasher>::Domain::try_from_bytes(&p_aux.comm_c.into_bytes())?,
@@ -158,14 +178,7 @@ pub fn decode_from<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHasher>>(
 ) -> Result<()> {
     info!("decode_from:start");
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let nodes_count = config.nodes_count;
     EmptySectorUpdate::<Tree>::decode_from(
@@ -197,14 +210,7 @@ pub fn remove_encoded_data<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHasher>
 ) -> Result<()> {
     info!("remove_data:start");
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = replica_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux = get_p_aux::<Tree>(replica_cache_path)?;
 
     let nodes_count = config.nodes_count;
     EmptySectorUpdate::<Tree>::remove_encoded_data(
@@ -245,14 +251,7 @@ pub fn generate_single_partition_proof<Tree: 'static + MerkleTreeTrait<Hasher = 
     let public_params: storage_proofs_update::PublicParams =
         PublicParams::from_sector_size(u64::from(config.sector_size));
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux_old: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux_old = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let public_inputs: storage_proofs_update::PublicInputs = PublicInputs {
         k: 0,
@@ -263,26 +262,16 @@ pub fn generate_single_partition_proof<Tree: 'static + MerkleTreeTrait<Hasher = 
         h: usize::from(config.h_select),
     };
 
-    // Note: t_aux has labels and tree_d, tree_c, tree_r_last store configs
-    let t_aux_old = {
-        let t_aux_path = sector_key_cache_path.join(CacheKey::TAux.to_string());
-        let t_aux_bytes = fs::read(&t_aux_path)
-            .with_context(|| format!("could not read file t_aux={:?}", t_aux_path))?;
+    let t_aux_old = get_t_aux::<Tree>(sector_key_cache_path)?;
 
-        let res: TemporaryAux<Tree, DefaultPieceHasher> = deserialize(&t_aux_bytes)?;
-        res
-    };
-
-    // Re-instantiate a t_aux with the new replica cache path, then
-    // use new tree_d_config and tree_r_last_config from it.
-    let mut t_aux_new = t_aux_old.clone();
-    t_aux_new.set_cache_path(replica_cache_path);
+    let (tree_d_new_config, tree_r_last_new_config) =
+        get_new_configs_from_t_aux_old::<Tree>(&t_aux_old, replica_cache_path, config.nodes_count)?;
 
     let private_inputs: PrivateInputs = PrivateInputs {
         tree_r_old_config: t_aux_old.tree_r_last_config,
         old_replica_path: sector_key_path.to_path_buf(),
-        tree_d_new_config: t_aux_new.tree_d_config,
-        tree_r_new_config: t_aux_new.tree_r_last_config,
+        tree_d_new_config,
+        tree_r_new_config: tree_r_last_new_config,
         replica_path: replica_path.to_path_buf(),
     };
 
@@ -313,14 +302,7 @@ pub fn verify_single_partition_proof<Tree: 'static + MerkleTreeTrait<Hasher = Tr
     let public_params: storage_proofs_update::PublicParams =
         PublicParams::from_sector_size(u64::from(config.sector_size));
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux_old: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux_old = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let public_inputs: storage_proofs_update::PublicInputs = PublicInputs {
         k: 0,
@@ -360,14 +342,7 @@ pub fn generate_partition_proofs<Tree: 'static + MerkleTreeTrait<Hasher = TreeRH
     let public_params: storage_proofs_update::PublicParams =
         PublicParams::from_sector_size(u64::from(config.sector_size));
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux_old: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux_old = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let public_inputs: storage_proofs_update::PublicInputs = PublicInputs {
         k: usize::from(config.update_partitions),
@@ -378,26 +353,16 @@ pub fn generate_partition_proofs<Tree: 'static + MerkleTreeTrait<Hasher = TreeRH
         h: usize::from(config.h_select),
     };
 
-    // Note: t_aux has labels and tree_d, tree_c, tree_r_last store configs
-    let t_aux_old = {
-        let t_aux_path = sector_key_cache_path.join(CacheKey::TAux.to_string());
-        let t_aux_bytes = fs::read(&t_aux_path)
-            .with_context(|| format!("could not read file t_aux={:?}", t_aux_path))?;
+    let t_aux_old = get_t_aux::<Tree>(sector_key_cache_path)?;
 
-        let res: TemporaryAux<Tree, DefaultPieceHasher> = deserialize(&t_aux_bytes)?;
-        res
-    };
-
-    // Re-instantiate a t_aux with the new replica cache path, then
-    // use new tree_d_config and tree_r_last_config from it.
-    let mut t_aux_new = t_aux_old.clone();
-    t_aux_new.set_cache_path(replica_cache_path);
+    let (tree_d_new_config, tree_r_last_new_config) =
+        get_new_configs_from_t_aux_old::<Tree>(&t_aux_old, replica_cache_path, config.nodes_count)?;
 
     let private_inputs: PrivateInputs = PrivateInputs {
         tree_r_old_config: t_aux_old.tree_r_last_config,
         old_replica_path: sector_key_path.to_path_buf(),
-        tree_d_new_config: t_aux_new.tree_d_config,
-        tree_r_new_config: t_aux_new.tree_r_last_config,
+        tree_d_new_config,
+        tree_r_new_config: tree_r_last_new_config,
         replica_path: replica_path.to_path_buf(),
     };
 
@@ -431,14 +396,7 @@ pub fn verify_partition_proofs<Tree: 'static + MerkleTreeTrait<Hasher = TreeRHas
     let public_params: storage_proofs_update::PublicParams =
         PublicParams::from_sector_size(u64::from(config.sector_size));
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux_old: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux_old = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let public_inputs: storage_proofs_update::PublicInputs = PublicInputs {
         k: usize::from(config.update_partitions),
@@ -478,29 +436,7 @@ pub fn generate_empty_sector_update_proof<Tree: 'static + MerkleTreeTrait<Hasher
 
     let config = SectorUpdateConfig::from_porep_config(porep_config);
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux_old: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
-
-    // Note: t_aux has labels and tree_d, tree_c, tree_r_last store configs
-    let t_aux_old = {
-        let t_aux_path = sector_key_cache_path.join(CacheKey::TAux.to_string());
-        let t_aux_bytes = fs::read(&t_aux_path)
-            .with_context(|| format!("could not read file t_aux={:?}", t_aux_path))?;
-
-        let res: TemporaryAux<Tree, DefaultPieceHasher> = deserialize(&t_aux_bytes)?;
-        res
-    };
-
-    // Re-instantiate a t_aux with the new replica cache path, then
-    // use new tree_d_config and tree_r_last_config from it.
-    let mut t_aux_new = t_aux_old.clone();
-    t_aux_new.set_cache_path(replica_cache_path);
+    let p_aux_old = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let partitions = usize::from(config.update_partitions);
     let public_inputs: storage_proofs_update::PublicInputs = PublicInputs {
@@ -512,11 +448,16 @@ pub fn generate_empty_sector_update_proof<Tree: 'static + MerkleTreeTrait<Hasher
         h: usize::from(config.h_select),
     };
 
+    let t_aux_old = get_t_aux::<Tree>(sector_key_cache_path)?;
+
+    let (tree_d_new_config, tree_r_last_new_config) =
+        get_new_configs_from_t_aux_old::<Tree>(&t_aux_old, replica_cache_path, config.nodes_count)?;
+
     let private_inputs: PrivateInputs = PrivateInputs {
         tree_r_old_config: t_aux_old.tree_r_last_config,
         old_replica_path: sector_key_path.to_path_buf(),
-        tree_d_new_config: t_aux_new.tree_d_config,
-        tree_r_new_config: t_aux_new.tree_r_last_config,
+        tree_d_new_config,
+        tree_r_new_config: tree_r_last_new_config,
         replica_path: replica_path.to_path_buf(),
     };
 
@@ -557,14 +498,7 @@ pub fn verify_empty_sector_update_proof<Tree: 'static + MerkleTreeTrait<Hasher =
 
     let comm_d_new_safe = DefaultPieceDomain::try_from_bytes(&comm_d_new)?;
 
-    // NOTE: p_aux has comm_c and comm_r_last
-    let p_aux_old: PersistentAux<<Tree::Hasher as Hasher>::Domain> = {
-        let p_aux_path = sector_key_cache_path.join(CacheKey::PAux.to_string());
-        let p_aux_bytes = fs::read(&p_aux_path)
-            .with_context(|| format!("could not read file p_aux={:?}", p_aux_path))?;
-
-        deserialize(&p_aux_bytes)
-    }?;
+    let p_aux_old = get_p_aux::<Tree>(sector_key_cache_path)?;
 
     let config = SectorUpdateConfig::from_porep_config(porep_config);
     let partitions = usize::from(config.update_partitions);
