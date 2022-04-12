a529fc1049d79d39c3621d9abb88fbe191b4937c
Volker Mische <volker.mische@gmail.com>
2022-04-12 14:53:07 +0200

    fix: use Neptune branch that also supports other fields

--- storage-proofs-porep/src/stacked/vanilla/proof.rs
+++ storage-proofs-porep/src/stacked/vanilla/proof.rs
@@ -638,7 +638,7 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> StackedDrg<'a, Tr
                             None
                         }
                     };
-                    let mut column_tree_builder = ColumnTreeBuilder::<ColumnArity, TreeArity>::new(
+                    let mut column_tree_builder = ColumnTreeBuilder::<Fr, ColumnArity, TreeArity>::new(
                         column_batcher,
                         tree_batcher,
                         nodes_count,
@@ -1083,7 +1083,7 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> StackedDrg<'a, Tr
                         None
                     }
                 };
-                let mut tree_builder = TreeBuilder::<Tree::Arity>::new(
+                let mut tree_builder = TreeBuilder::<Fr, Tree::Arity>::new(
                     batcher,
                     nodes_count,
                     tree_r_last_config.rows_to_discard,
@@ -1543,7 +1543,7 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> StackedDrg<'a, Tr
                     None
                 }
             };
-            let mut tree_builder = TreeBuilder::<Tree::Arity>::new(
+            let mut tree_builder = TreeBuilder::<Fr, Tree::Arity>::new(
                 batcher,
                 nodes_count,
                 tree_r_last_config.rows_to_discard,
