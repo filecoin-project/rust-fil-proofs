49848bc129f4a7fe1b80ac57b0eec380376b4978
DrPeterVanNostrand <jnz@riseup.net>
2022-05-25 19:23:21 +0000

    feat: Winning and Window-PoSt halo2 circuits

---
+++ storage-proofs-post/src/halo2/shared.rs
@@ -0,0 +1,322 @@
+use std::any::TypeId;
+use std::convert::TryInto;
+use std::iter;
+use std::marker::PhantomData;
+use std::mem;
+
+use fil_halo2_gadgets::{
+    uint32::{UInt32Chip, UInt32Config},
+    ColumnBuilder,
+};
+use filecoin_hashers::{poseidon::PoseidonHasher, Domain, HaloHasher, Hasher, PoseidonArity};
+use generic_array::typenum::U2;
+use halo2_proofs::{
+    arithmetic::FieldExt,
+    plonk::{Advice, Column, ConstraintSystem, Instance},
+};
+use storage_proofs_core::{
+    gadgets::halo2::{
+        insert::{InsertChip, InsertConfig},
+        por::{self, MerkleChip},
+    },
+    merkle::MerkleProofTrait,
+};
+
+use crate::fallback as vanilla;
+
+// Circuit private inputs for a challenged sector.
+#[derive(Clone)]
+pub struct SectorProof<F, U, V, W, const SECTOR_NODES: usize, const SECTOR_CHALLENGES: usize>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    pub comm_c: Option<F>,
+    pub root_r: Option<F>,
+    pub leafs_r: [Option<F>; SECTOR_CHALLENGES],
+    pub paths_r: [Vec<Vec<Option<F>>>; SECTOR_CHALLENGES],
+    pub _tree_r: PhantomData<(U, V, W)>,
+}
+
+impl<F, U, V, W, P, const SECTOR_NODES: usize, const SECTOR_CHALLENGES: usize>
+    From<&vanilla::SectorProof<P>> for SectorProof<F, U, V, W, SECTOR_NODES, SECTOR_CHALLENGES>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+    P: MerkleProofTrait<Hasher = PoseidonHasher<F>, Arity = U, SubTreeArity = V, TopTreeArity = W>,
+{
+    #[allow(clippy::unwrap_used)]
+    fn from(vanilla_proof: &vanilla::SectorProof<P>) -> Self {
+        assert_eq!(vanilla_proof.inclusion_proofs.len(), SECTOR_CHALLENGES);
+
+        let mut leafs_r = Vec::with_capacity(SECTOR_CHALLENGES);
+        let mut paths_r = Vec::with_capacity(SECTOR_CHALLENGES);
+
+        for merkle_proof in vanilla_proof.inclusion_proofs.iter() {
+            let leaf_r: Option<F> = Some(merkle_proof.leaf().into());
+            let path_r: Vec<Vec<Option<F>>> = merkle_proof
+                .path()
+                .iter()
+                .map(|(siblings, _)| siblings.iter().map(|&sib| Some(sib.into())).collect())
+                .collect();
+            leafs_r.push(leaf_r);
+            paths_r.push(path_r);
+        }
+
+        SectorProof {
+            comm_c: Some(vanilla_proof.comm_c.into()),
+            root_r: Some(vanilla_proof.comm_r_last.into()),
+            leafs_r: leafs_r.try_into().unwrap(),
+            paths_r: paths_r.try_into().unwrap(),
+            _tree_r: PhantomData,
+        }
+    }
+}
+
+impl<F, U, V, W, P, const SECTOR_NODES: usize, const SECTOR_CHALLENGES: usize>
+    From<vanilla::SectorProof<P>> for SectorProof<F, U, V, W, SECTOR_NODES, SECTOR_CHALLENGES>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+    P: MerkleProofTrait<Hasher = PoseidonHasher<F>, Arity = U, SubTreeArity = V, TopTreeArity = W>,
+{
+    fn from(vanilla_proof: vanilla::SectorProof<P>) -> Self {
+        Self::from(&vanilla_proof)
+    }
+}
+
+impl<F, U, V, W, const SECTOR_NODES: usize, const SECTOR_CHALLENGES: usize>
+    SectorProof<F, U, V, W, SECTOR_NODES, SECTOR_CHALLENGES>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    #[allow(clippy::unwrap_used)]
+    pub fn empty() -> Self {
+        SectorProof {
+            comm_c: None,
+            root_r: None,
+            leafs_r: [None; SECTOR_CHALLENGES],
+            paths_r: iter::repeat(por::empty_path::<F, U, V, W, SECTOR_NODES>())
+                .take(SECTOR_CHALLENGES)
+                .collect::<Vec<Vec<Vec<Option<F>>>>>()
+                .try_into()
+                .unwrap(),
+            _tree_r: PhantomData,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct CircuitConfig<F, U, V, W, const SECTOR_NODES: usize>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    // Decomposes each Merkle challenge public input into 32 bits.
+    pub uint32: UInt32Config<F>,
+    // Computes `comm_r`.
+    pub poseidon_2: <PoseidonHasher<F> as HaloHasher<U2>>::Config,
+    // Computes TreeR root from each challenge's Merkle proof.
+    #[allow(clippy::type_complexity)]
+    pub tree_r: (
+        <PoseidonHasher<F> as HaloHasher<U>>::Config,
+        InsertConfig<F, U>,
+        Option<(
+            <PoseidonHasher<F> as HaloHasher<V>>::Config,
+            InsertConfig<F, V>,
+        )>,
+        Option<(
+            <PoseidonHasher<F> as HaloHasher<W>>::Config,
+            InsertConfig<F, W>,
+        )>,
+    ),
+    // Equality enabled columns.
+    pub advice: [Column<Advice>; 2],
+    pub pi: Column<Instance>,
+}
+
+impl<F, U, V, W, const SECTOR_NODES: usize> CircuitConfig<F, U, V, W, SECTOR_NODES>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    #[allow(clippy::unwrap_used)]
+    pub fn configure(meta: &mut ConstraintSystem<F>) -> Self {
+        let (advice_eq, advice_neq, fixed_eq, fixed_neq) = ColumnBuilder::new()
+            .with_chip::<UInt32Chip<F>>()
+            .with_chip::<<PoseidonHasher<F> as HaloHasher<U2>>::Chip>()
+            .with_chip::<<PoseidonHasher<F> as HaloHasher<U>>::Chip>()
+            .with_chip::<InsertChip<F, U>>()
+            .create_columns(meta);
+
+        let uint32 = UInt32Chip::configure(meta, advice_eq[..9].try_into().unwrap());
+
+        let poseidon_2 = <PoseidonHasher<F> as HaloHasher<U2>>::configure(
+            meta,
+            &advice_eq,
+            &advice_neq,
+            &fixed_eq,
+            &fixed_neq,
+        );
+
+        let binary_arity_type = TypeId::of::<U2>();
+        let base_arity_type = TypeId::of::<U>();
+        let sub_arity_type = TypeId::of::<V>();
+        let top_arity_type = TypeId::of::<W>();
+
+        let sub_arity = V::to_usize();
+        let top_arity = W::to_usize();
+
+        let poseidon_base = if base_arity_type == binary_arity_type {
+            unsafe { mem::transmute(poseidon_2.clone()) }
+        } else {
+            <PoseidonHasher<F> as HaloHasher<U>>::configure(
+                meta,
+                &advice_eq,
+                &advice_neq,
+                &fixed_eq,
+                &fixed_neq,
+            )
+        };
+
+        let insert_base = InsertChip::<F, U>::configure(meta, &advice_eq, &advice_neq);
+
+        let poseidon_sub = if sub_arity == 0 {
+            None
+        } else if sub_arity_type == binary_arity_type {
+            unsafe { Some(mem::transmute(poseidon_2.clone())) }
+        } else if sub_arity_type == base_arity_type {
+            unsafe { Some(mem::transmute(poseidon_base.clone())) }
+        } else {
+            Some(<PoseidonHasher<F> as HaloHasher<V>>::configure(
+                meta,
+                &advice_eq,
+                &advice_neq,
+                &fixed_eq,
+                &fixed_neq,
+            ))
+        };
+
+        let insert_sub = if sub_arity == 0 {
+            None
+        } else if sub_arity_type == base_arity_type {
+            unsafe { Some(mem::transmute(insert_base.clone())) }
+        } else {
+            Some(InsertChip::<F, V>::configure(meta, &advice_eq, &advice_neq))
+        };
+
+        let poseidon_top = if top_arity == 0 {
+            None
+        } else if top_arity_type == binary_arity_type {
+            unsafe { Some(mem::transmute(poseidon_2.clone())) }
+        } else if top_arity_type == base_arity_type {
+            unsafe { Some(mem::transmute(poseidon_base.clone())) }
+        } else if top_arity_type == sub_arity_type {
+            unsafe { Some(mem::transmute(poseidon_sub.clone())) }
+        } else {
+            Some(<PoseidonHasher<F> as HaloHasher<W>>::configure(
+                meta,
+                &advice_eq,
+                &advice_neq,
+                &fixed_eq,
+                &fixed_neq,
+            ))
+        };
+
+        let insert_top = if top_arity == 0 {
+            None
+        } else if top_arity_type == base_arity_type {
+            unsafe { Some(mem::transmute(insert_base.clone())) }
+        } else if top_arity_type == sub_arity_type {
+            unsafe { Some(mem::transmute(insert_sub.clone())) }
+        } else {
+            Some(InsertChip::<F, W>::configure(meta, &advice_eq, &advice_neq))
+        };
+
+        let pi = meta.instance_column();
+        meta.enable_equality(pi);
+
+        CircuitConfig {
+            uint32,
+            poseidon_2,
+            tree_r: (
+                poseidon_base,
+                insert_base,
+                poseidon_sub.zip(insert_sub),
+                poseidon_top.zip(insert_top),
+            ),
+            advice: [advice_eq[0], advice_eq[1]],
+            pi,
+        }
+    }
+
+    pub fn construct_chips(
+        self,
+    ) -> (
+        UInt32Chip<F>,
+        <PoseidonHasher<F> as HaloHasher<U2>>::Chip,
+        MerkleChip<PoseidonHasher<F>, U, V, W>,
+    ) {
+        let CircuitConfig {
+            uint32: uint32_config,
+            poseidon_2: poseidon_2_config,
+            tree_r: (poseidon_base_config, insert_base_config, sub_config, top_config),
+            ..
+        } = self;
+
+        let uint32_chip = UInt32Chip::construct(uint32_config);
+
+        let poseidon_2_chip = <PoseidonHasher<F> as HaloHasher<U2>>::construct(poseidon_2_config);
+
+        let tree_r_merkle_chip = {
+            let poseidon_base_chip =
+                <PoseidonHasher<F> as HaloHasher<U>>::construct(poseidon_base_config);
+            let insert_base_chip = InsertChip::construct(insert_base_config);
+            let sub_chips = sub_config.map(|(poseidon_sub, insert_sub)| {
+                (
+                    <PoseidonHasher<F> as HaloHasher<V>>::construct(poseidon_sub),
+                    InsertChip::construct(insert_sub),
+                )
+            });
+            let top_chips = top_config.map(|(poseidon_top, insert_top)| {
+                (
+                    <PoseidonHasher<F> as HaloHasher<W>>::construct(poseidon_top),
+                    InsertChip::construct(insert_top),
+                )
+            });
+            MerkleChip::<PoseidonHasher<F>, U, V, W>::with_subchips(
+                poseidon_base_chip,
+                insert_base_chip,
+                sub_chips,
+                top_chips,
+            )
+        };
+
+        (uint32_chip, poseidon_2_chip, tree_r_merkle_chip)
+    }
+}
---
+++ storage-proofs-post/src/halo2/winning.rs
@@ -0,0 +1,257 @@
+use std::convert::TryInto;
+use std::ops::RangeInclusive;
+
+use filecoin_hashers::{
+    poseidon::PoseidonHasher, Domain, FieldArity, Hasher, PoseidonArity, POSEIDON_CONSTANTS,
+};
+use generic_array::typenum::U2;
+use halo2_proofs::{
+    arithmetic::FieldExt,
+    circuit::{Layouter, SimpleFloorPlanner},
+    plonk::{Circuit, ConstraintSystem, Error},
+};
+use sha2::{Digest, Sha256};
+use storage_proofs_core::halo2_proofs::CircuitRows;
+
+use crate::{
+    fallback::{self as vanilla, SetupParams},
+    halo2::shared::{CircuitConfig, SectorProof},
+};
+
+pub const CHALLENGE_COUNT: usize = 66;
+
+// Absolute rows of public inputs.
+const COMM_R_ROW: usize = 0;
+const CHALLENGE_ROWS: RangeInclusive<usize> = 1..=CHALLENGE_COUNT;
+
+#[allow(clippy::unwrap_used)]
+pub fn generate_challenges<F: FieldExt, const SECTOR_NODES: usize>(
+    randomness: F,
+    sector_id: u64,
+    k: usize,
+) -> [u32; CHALLENGE_COUNT] {
+    let sector_nodes = SECTOR_NODES as u64;
+    let mut hasher = Sha256::new();
+    hasher.update(randomness.to_repr().as_ref());
+    hasher.update(sector_id.to_le_bytes());
+
+    let mut challenges = [0u32; CHALLENGE_COUNT];
+
+    for (i, challenge) in challenges.iter_mut().enumerate() {
+        let challenge_index = (k * CHALLENGE_COUNT + i) as u64;
+        let mut hasher = hasher.clone();
+        hasher.update(&challenge_index.to_le_bytes());
+        let digest = hasher.finalize();
+        let uint64 = u64::from_le_bytes(digest[..8].try_into().unwrap());
+        *challenge = (uint64 % sector_nodes) as u32;
+    }
+
+    challenges
+}
+
+#[derive(Clone)]
+pub struct PublicInputs<F, const SECTOR_NODES: usize>
+where
+    F: FieldExt,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    pub comm_r: Option<F>,
+    pub challenges: [Option<u32>; CHALLENGE_COUNT],
+}
+
+impl<F, const SECTOR_NODES: usize> PublicInputs<F, SECTOR_NODES>
+where
+    F: FieldExt,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    #[allow(clippy::unwrap_used)]
+    pub fn from(
+        setup_params: SetupParams,
+        vanilla_pub_inputs: vanilla::PublicInputs<<PoseidonHasher<F> as Hasher>::Domain>,
+    ) -> Self {
+        assert_eq!(setup_params.sector_size >> 5, SECTOR_NODES as u64);
+        assert_eq!(setup_params.challenge_count, CHALLENGE_COUNT);
+        assert_eq!(setup_params.sector_count, 1);
+        assert_eq!(vanilla_pub_inputs.sectors.len(), 1);
+
+        let randomness: F = vanilla_pub_inputs.randomness.into();
+        let sector_id: u64 = vanilla_pub_inputs.sectors[0].id.into();
+        let comm_r: F = vanilla_pub_inputs.sectors[0].comm_r.into();
+        let k = vanilla_pub_inputs.k.unwrap_or(0);
+
+        let challenges = generate_challenges::<F, SECTOR_NODES>(randomness, sector_id, k)
+            .iter()
+            .copied()
+            .map(Some)
+            .collect::<Vec<Option<u32>>>()
+            .try_into()
+            .unwrap();
+
+        PublicInputs {
+            comm_r: Some(comm_r),
+            challenges,
+        }
+    }
+
+    pub fn empty() -> Self {
+        PublicInputs {
+            comm_r: None,
+            challenges: [None; CHALLENGE_COUNT],
+        }
+    }
+
+    #[allow(clippy::unwrap_used)]
+    pub fn to_vec(&self) -> Vec<Vec<F>> {
+        assert!(self.comm_r.is_some() && self.challenges.iter().all(Option::is_some));
+        let mut pub_inputs = Vec::with_capacity(1 + CHALLENGE_COUNT);
+        pub_inputs.push(self.comm_r.unwrap());
+        for c in &self.challenges {
+            pub_inputs.push(F::from(c.unwrap() as u64));
+        }
+        vec![pub_inputs]
+    }
+}
+
+pub type PrivateInputs<F, U, V, W, const SECTOR_NODES: usize> =
+    SectorProof<F, U, V, W, SECTOR_NODES, CHALLENGE_COUNT>;
+
+#[derive(Clone)]
+pub struct WinningPostCircuit<F, U, V, W, const SECTOR_NODES: usize>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    pub pub_inputs: PublicInputs<F, SECTOR_NODES>,
+    pub priv_inputs: PrivateInputs<F, U, V, W, SECTOR_NODES>,
+}
+
+impl<F, U, V, W, const SECTOR_NODES: usize> Circuit<F>
+    for WinningPostCircuit<F, U, V, W, SECTOR_NODES>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    type Config = CircuitConfig<F, U, V, W, SECTOR_NODES>;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        WinningPostCircuit {
+            pub_inputs: PublicInputs::empty(),
+            priv_inputs: PrivateInputs::empty(),
+        }
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        CircuitConfig::configure(meta)
+    }
+
+    #[allow(clippy::unwrap_used)]
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), Error> {
+        let WinningPostCircuit { priv_inputs, .. } = self;
+
+        let advice = config.advice;
+        let pi_col = config.pi;
+
+        let (uint32_chip, poseidon_2_chip, tree_r_merkle_chip) = config.construct_chips();
+
+        // Witness `comm_c` and `root_r`.
+        let (comm_c, root_r) = layouter.assign_region(
+            || "witness comm_c and root_r",
+            |mut region| {
+                let offset = 0;
+                let comm_c = region.assign_advice(
+                    || "comm_c",
+                    advice[0],
+                    offset,
+                    || priv_inputs.comm_c.ok_or(Error::Synthesis),
+                )?;
+
+                let root_r = region.assign_advice(
+                    || "root_r",
+                    advice[1],
+                    offset,
+                    || priv_inputs.root_r.ok_or(Error::Synthesis),
+                )?;
+
+                Ok((comm_c, root_r))
+            },
+        )?;
+
+        // Compute `comm_r = H(comm_c, root_r)` and constrain with public input.
+        let comm_r = poseidon_2_chip.hash(
+            layouter.namespace(|| "calculate comm_r"),
+            &[comm_c, root_r.clone()],
+            POSEIDON_CONSTANTS.get::<FieldArity<F, U2>>().unwrap(),
+        )?;
+        layouter.constrain_instance(comm_r.cell(), pi_col, COMM_R_ROW)?;
+
+        for (i, ((leaf_r, path_r), challenge_row)) in priv_inputs
+            .leafs_r
+            .iter()
+            .zip(priv_inputs.paths_r.iter())
+            .zip(CHALLENGE_ROWS)
+            .enumerate()
+        {
+            // Assign the challenge as 32 bits and constrain with public input.
+            let challenge_bits = uint32_chip.pi_assign_bits(
+                layouter.namespace(|| {
+                    format!("challenge {} assign challenge public input as 32 bits", i,)
+                }),
+                pi_col,
+                challenge_row,
+            )?;
+
+            // Verify the challenge's TreeR Merkle proof.
+            let root_r_calc = tree_r_merkle_chip.compute_root(
+                layouter
+                    .namespace(|| format!("challenge {} calculate comm_r from merkle proof", i)),
+                &challenge_bits,
+                leaf_r,
+                path_r,
+            )?;
+            layouter.assign_region(
+                || format!("challenge {} constrain root_r_calc", i),
+                |mut region| region.constrain_equal(root_r_calc.cell(), root_r.cell()),
+            )?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<F, U, V, W, const SECTOR_NODES: usize> CircuitRows
+    for WinningPostCircuit<F, U, V, W, SECTOR_NODES>
+where
+    F: FieldExt,
+    U: PoseidonArity<F>,
+    V: PoseidonArity<F>,
+    W: PoseidonArity<F>,
+    PoseidonHasher<F>: Hasher,
+    <PoseidonHasher<F> as Hasher>::Domain: Domain<Field = F>,
+{
+    fn k(&self) -> u32 {
+        use crate::halo2::constants::*;
+        match SECTOR_NODES {
+            SECTOR_NODES_2_KIB => 13,
+            SECTOR_NODES_4_KIB => 13,
+            SECTOR_NODES_16_KIB => 13,
+            SECTOR_NODES_32_KIB => 14,
+            // TODO (jake): add more sector sizes
+            _ => unimplemented!(),
+        }
+    }
+}
