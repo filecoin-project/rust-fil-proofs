8fb7fc395025c91de4ff046e4cdfa1694f64c28c
nemo <nemo@protocol.ai>
2022-09-01 13:37:27 -0400

    feat: add missing halo2paramcache

--- filecoin-proofs/src/types/post_config.rs
+++ filecoin-proofs/src/types/post_config.rs
@@ -49,6 +49,13 @@ impl From<PoStConfig> for UnpaddedBytesAmount {
     }
 }
 
+impl From<PoStConfig> for SectorSize {
+    fn from(x: PoStConfig) -> Self {
+        let PoStConfig { sector_size, .. } = x;
+        sector_size
+    }
+}
+
 impl PoStConfig {
     pub fn new_halo2(
         sector_size: SectorSize,
