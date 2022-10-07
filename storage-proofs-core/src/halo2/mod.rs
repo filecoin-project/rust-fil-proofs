47d22fab753a6e2f438451407be2ec431c14e50e
DrPeterVanNostrand <jnz@riseup.net>
2022-10-07 19:07:47 +0000

    refactor: remove lazy_static halo2 keystore

--- storage-proofs-core/src/halo2/mod.rs
+++ storage-proofs-core/src/halo2/mod.rs
@@ -3,6 +3,6 @@ pub mod gadgets;
 mod proof;
 
 pub use proof::{
-    create_batch_proof, create_proof, halo2_keystore, verify_batch_proof, verify_proof,
-    verify_proofs, CircuitRows, CompoundProof, Halo2Field, Halo2Keypair, Halo2Proof,
+    create_batch_proof, create_proof, verify_batch_proof, verify_proof, verify_proofs, CircuitRows,
+    CompoundProof, Halo2Field, Halo2Keypair, Halo2Proof,
 };
