e03807da5f638ec6854750f51a4517c09b57e059
Artem Storozhuk <storojs72@gmail.com>
2022-12-14 15:37:52 +0200

    doc(fil-halo2-gadgets): Add documentation for AdviceIter abstraction

--- fil-halo2-gadgets/src/lib.rs
+++ fil-halo2-gadgets/src/lib.rs
@@ -175,29 +175,18 @@ impl AdviceIter {
     /// Returns next `(offset, column)` tuple, considering previous data assignment.
     ///
     /// ```
-    ///  use pasta_curves::Fp;
-    ///  use halo2_proofs::plonk::{ ConstraintSystem, Column, Advice };
-    ///  use fil_halo2_gadgets::AdviceIter;
+    ///  // Consider `advice_columns` to be `[Column<Advice>; 8]`
     ///
-    ///  let mut cs = ConstraintSystem::<Fp>::default();
-    ///
-    ///  let advice_columns = (0..8).into_iter().map(|_| cs.advice_column()).collect::<Vec<Column<Advice>>>();
-    ///
-    ///  let mut advice_iter = AdviceIter::from(advice_columns);
+    ///  let mut advice_iter = AdviceIter::from(advice_columns.to_vec());
     ///
     ///  for index in 0..8 {
-    ///     let (offset, col) = advice_iter.next();
-    ///     assert_eq!(offset, 0);
-    ///  }
-    ///
-    ///  for index in 8..16 {
-    ///     let (offset, col) = advice_iter.next();
-    ///     assert_eq!(offset, 1);
-    ///  }
-    ///
-    ///  for index in 16..24 {
-    ///     let (offset, col) = advice_iter.next();
-    ///     assert_eq!(offset, 2);
+    ///      let (offset, col) = advice_iter.next();
+    ///           assert_eq!(offset, 0);
+    ///      }
+    ///      for index in 8..16 {
+    ///           let (offset, col) = advice_iter.next();
+    ///           assert_eq!(offset, 1);
+    ///      }
     ///  }
     /// ```
     #[allow(clippy::should_implement_trait)]
