646f162bafdfcae74ab57d0406d298b57eb75d40
Volker Mische <volker.mische@gmail.com>
2022-12-22 14:51:33 +0100

    docs: fix wrong comments

--- storage-proofs-core/src/merkle/mod.rs
+++ storage-proofs-core/src/merkle/mod.rs
@@ -18,14 +18,13 @@ pub use tree::*;
 
 /// A tree that is fully persisted to disk.
 ///
-/// It's generic over the hash function `H`, the base arity `U`, sub-tree arity `V` and top tree
+/// It's generic over the hash function `H`, the base arity `U`, sub-tree arity `V` and top-tree
 /// arity `W`.
 ///
-/// The base arity is used for the leaves. If all other arities are zero, then this arity is used
-/// for all levels.
-/// The sub-tree arity is for all levels between the level above the leaves and the level below the
-/// root (which is always a single item).
-/// The top-tree arity is used for the top level that then results in the root node.
+/// The base arity is used for for all levels up to the top. Non zero arties of the top-tree and/or
+/// sub-tree each add another layer on top. So a tree with e.g. `U = 8`, `V = 4`, `W = 2` would
+/// create a tree where the top level has two children, the levels below 4 children and all other
+/// levels below have 8 children.
 pub type DiskTree<H, U, V, W> = MerkleTreeWrapper<H, DiskStore<<H as Hasher>::Domain>, U, V, W>;
 
 /// A tree that is partially stored on disk, some levels are in memory.
@@ -33,11 +32,10 @@ pub type DiskTree<H, U, V, W> = MerkleTreeWrapper<H, DiskStore<<H as Hasher>::Do
 /// It's generic over the hash function `H`, the base arity `U`, sub-tree arity `V` and top tree
 /// arity `W`.
 ///
-/// The base arity is used for the leaves. If all other arities are zero, then this arity is used
-/// for all levels.
-/// The sub-tree arity is for all levels between the level above the leaves and the level below the
-/// root (which is always a single item).
-/// The top-tree arity is used for the top level that then results in the root node.
+/// The base arity is used for for all levels up to the top. Non zero arties of the top-tree and/or
+/// sub-tree each add another layer on top. So a tree with e.g. `U = 8`, `V = 4`, `W = 2` would
+/// create a tree where the top level has two children, the levels below 4 children and all other
+/// levels below have 8 children.
 pub type LCTree<H, U, V, W> =
     MerkleTreeWrapper<H, LevelCacheStore<<H as Hasher>::Domain, File>, U, V, W>;
 
