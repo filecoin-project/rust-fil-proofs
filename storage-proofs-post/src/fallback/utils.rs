a613cf473d7e4ed3d9defa0b2178dbb9410bf18c
nemo <nemo@protocol.ai>
2023-03-01 09:15:29 -0500

    fix: apply review feedback

--- storage-proofs-post/src/fallback/utils.rs
+++ storage-proofs-post/src/fallback/utils.rs
@@ -8,9 +8,9 @@ pub fn get_challenge_index(
     challenge_index: usize,
 ) -> u64 {
     (match api_version {
-        ApiVersion::V1_2_0 => challenge_index,
         ApiVersion::V1_0_0 | ApiVersion::V1_1_0 => {
             sector_index * challenge_count_per_sector + challenge_index
         }
+        ApiVersion::V1_2_0 => challenge_index,
     } as u64)
 }
