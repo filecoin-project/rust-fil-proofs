6663aaf8fe5d029e4c7e57094f86fe8cb4fba025
DrPeterVanNostrand <jnz@riseup.net>
2023-04-13 17:19:55 +0000

    uncomment lifecycle tests

--- filecoin-proofs/tests/api.rs
+++ filecoin-proofs/tests/api.rs
@@ -114,7 +114,7 @@ fn test_seal_lifecycle_2kib_base_8() -> Result<()> {
 
     Ok(())
 }
-/*
+
 #[test]
 #[ignore]
 fn test_seal_lifecycle_upgrade_2kib_base_8() -> Result<()> {
@@ -133,7 +133,7 @@ fn test_seal_lifecycle_upgrade_2kib_base_8() -> Result<()> {
 
     Ok(())
 }
-*/
+
 #[test]
 #[ignore]
 fn test_seal_lifecycle_4kib_base_8() -> Result<()> {
@@ -154,7 +154,7 @@ fn test_seal_lifecycle_4kib_base_8() -> Result<()> {
 
     Ok(())
 }
-/*
+
 #[test]
 #[ignore]
 fn test_seal_lifecycle_upgrade_4kib_base_8() -> Result<()> {
@@ -170,14 +170,14 @@ fn test_seal_lifecycle_upgrade_4kib_base_8() -> Result<()> {
 
     Ok(())
 }
-*/
+
 #[test]
 #[ignore]
 fn test_seal_lifecycle_16kib_base_8() -> Result<()> {
     let test_inputs = vec![
-        //        (ARBITRARY_POREP_ID_V1_0_0, ApiVersion::V1_0_0, false),
-        //        (ARBITRARY_POREP_ID_V1_1_0, ApiVersion::V1_1_0, false),
-        //        (ARBITRARY_POREP_ID_V1_2_0, ApiVersion::V1_2_0, false),
+        (ARBITRARY_POREP_ID_V1_0_0, ApiVersion::V1_0_0, false),
+        (ARBITRARY_POREP_ID_V1_1_0, ApiVersion::V1_1_0, false),
+        (ARBITRARY_POREP_ID_V1_2_0, ApiVersion::V1_2_0, false),
         (ARBITRARY_POREP_ID_V1_2_0, ApiVersion::V1_2_0, true),
     ];
 
@@ -191,7 +191,7 @@ fn test_seal_lifecycle_16kib_base_8() -> Result<()> {
 
     Ok(())
 }
-/*
+
 #[test]
 #[ignore]
 fn test_seal_lifecycle_upgrade_16kib_base_8() -> Result<()> {
@@ -207,7 +207,6 @@ fn test_seal_lifecycle_upgrade_16kib_base_8() -> Result<()> {
 
     Ok(())
 }
- */
 
 #[test]
 #[ignore]
@@ -230,7 +229,6 @@ fn test_seal_lifecycle_32kib_base_8() -> Result<()> {
     Ok(())
 }
 
-/*
 // These tests are good to run, but take a long time.
 
 #[cfg(feature = "big-tests")]
@@ -288,7 +286,6 @@ fn test_seal_lifecycle_32gib_porep_id_v1_1_top_8_8_0_api_v1_1() -> Result<()> {
     assert!(!is_legacy_porep_id(porep_id));
     seal_lifecycle::<SectorShape32GiB>(SECTOR_SIZE_32_GIB, &porep_id, ApiVersion::V1_1_0)
 }
-*/
 
 #[cfg(feature = "big-tests")]
 #[test]
@@ -304,7 +301,7 @@ fn test_seal_lifecycle_32gib_porep_id_v1_1_top_8_8_0_api_v1_2() -> Result<()> {
 
     seal_lifecycle::<SectorShape32GiB>(&porep_config)
 }
-/*
+
 #[cfg(feature = "big-tests")]
 #[test]
 fn test_seal_lifecycle_upgrade_32gib_top_8_8_0_v1_1() -> Result<()> {
@@ -346,7 +343,6 @@ fn test_seal_lifecycle_upgrade_64gib_top_8_8_2_v1_1() -> Result<()> {
         ApiVersion::V1_1_0,
     )
 }
- */
 
 fn seal_lifecycle<Tree: 'static + MerkleTreeTrait>(porep_config: &PoRepConfig) -> Result<()> {
     let mut rng = XorShiftRng::from_seed(TEST_SEED);
