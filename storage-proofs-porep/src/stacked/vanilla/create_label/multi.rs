5942946a4cc226c19e9c58307727f8dac1b016e5
Volker Mische <volker.mische@gmail.com>
2023-05-16 17:17:41 +0200

    refactor: introduce new from trait for PoseidonDomain (#1703)
    Add a new `From` conversion into the field element repr representation.
    This makes it also possible to hide the internal representation.

--- storage-proofs-porep/src/stacked/vanilla/create_label/multi.rs
+++ storage-proofs-porep/src/stacked/vanilla/create_label/multi.rs
@@ -754,6 +754,6 @@ mod tests {
             .read_at(final_labels.len() - 1)
             .expect("read_at");
         dbg!(&last_label);
-        assert_eq!(expected_last_label.to_repr(), last_label.0);
+        assert_eq!(expected_last_label.to_repr(), last_label.as_ref());
     }
 }
